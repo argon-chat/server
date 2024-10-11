b15f51714c50bf83a8debb2c0de8c2ab48b7772f
Aram <aramhrptn@hotmail.com>
2024-10-12 03:58:52 +0400

    Add Microsoft Orleans and ServiceDefaults integration

--- src/Argon.Api/Program.cs
+++ src/Argon.Api/Program.cs
@@ -2,33 +2,22 @@ namespace Argon.Api;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
+        builder.AddServiceDefaults();
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
         app.UseHttpsRedirection();
-
         app.UseAuthorization();
-
-
         app.MapControllers();
-
-        app.Run();
+        await app.RunAsync();
     }
 }
