d2da6d28f254034368cafca7e7952d328571ac47
Aram <aramhrptn@hotmail.com>
2024-10-12 04:03:09 +0400

    Add Redis and PostgreSQL integration with Argon.Api

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -1,3 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cache = builder.AddRedis("cache");
+var username = builder.AddParameter("username", true);
+var password = builder.AddParameter("password", true);
+var db = builder.AddPostgres("pg", port: 5432, userName: username, password: password)
+        .WithDataVolume();
+
+var apiDb = db.AddDatabase("api");
+
+var api = builder.AddProject<Projects.Argon_Api>("Argon.Api")
+        .WithReference(apiDb, "DefaultConnection")
+        .WithReference(cache)
+        .WithExternalHttpEndpoints();
+
 builder.Build().Run();
