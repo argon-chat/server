1aab9473e0c5b5e7a6ca590b89ec584916389b89
Aram <aramhrptn@hotmail.com>
2024-10-15 18:51:57 +0400

    Add Identity framework

---
+++ src/Argon.Api/Entities/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Argon.Api.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    
+}
---
+++ src/Argon.Api/Services/EmailSender.cs
@@ -0,0 +1,29 @@
+using Argon.Api.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Argon.Api.Services;
+
+public class EmailSender(ILogger<EmailSender> logger) : IEmailSender<ApplicationUser>
+{
+    public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+    {
+        logger.LogInformation(
+            "Sending confirmation link to {email} for user {user} with confirmation link {confirmationLink}", email,
+            user.UserName, confirmationLink);
+        return Task.CompletedTask;
+    }
+
+    public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+    {
+        logger.LogInformation("Sending password reset link to {email} for user {user} with reset link {resetLink}",
+            email, user.UserName, resetLink);
+        return Task.CompletedTask;
+    }
+
+    public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+    {
+        logger.LogInformation("Sending password reset code to {email} for user {user} with reset code {resetCode}",
+            email, user.UserName, resetCode);
+        return Task.CompletedTask;
+    }
+}
