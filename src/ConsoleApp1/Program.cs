93e5cf4e1aa375ac7a26fe14a5d2b08a2144f76a
Aram <aramhrptn@hotmail.com>
2024-10-16 06:49:33 +0400

    Add ConsoleApp1 project and integrate with Orleans client

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Argon.Orleans.Client;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .CreateOrleansClient()
+            .Build();
+        
+        await host.StartAsync();
+        
+        var client = host.OrleansClient();
+
+        var result = client.SayHello();
+        
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        Console.WriteLine(result);
+        
+        await host.StopAsync();
+    }
+}
