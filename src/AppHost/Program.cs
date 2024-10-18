64ffaf5e630fb6cbf45916bc9a084bc6415f95a9
Yuuki Wesp <ls-micro@ya.ru>
2024-10-19 02:53:35 +0300

    add sfu creds

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -4,6 +4,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var username = builder.AddParameter("username", true);
 var password = builder.AddParameter("password", true);
+var sfuUrl = builder.AddParameter("sfu-url", true);
+var sfuClientId = builder.AddParameter("sfu-client-id", true);
+var sfuClientSecret = builder.AddParameter("sfu-client-secret", true);
 
 var cache = builder.AddRedis("cache", 6379);
 var rmq = builder.AddRabbitMQ("rmq", port: 5672, userName: username, password: password)
@@ -18,6 +21,9 @@ var api = builder.AddProject<Argon_Api>("argon-api")
     .WithReference(apiDb, "DefaultConnection")
     .WithReference(cache)
     .WithReference(rmq)
+    .WithEnvironment("sfu__url", sfuUrl)
+    .WithEnvironment("sfu__clientId", sfuClientId)
+    .WithEnvironment("sfu__clientSecret", sfuClientSecret)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
