0260c073ece8750c4db4491a450d3d5e102983cc
Yuuki Wesp <ls-micro@ya.ru>
2024-10-19 02:56:37 +0300

    add SfuFeature with initial services and other shit

---
+++ src/Argon.Api/Features/Sfu/SfuFeature.cs
@@ -0,0 +1,37 @@
+﻿namespace Argon.Sfu;
+
+using Flurl.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Flurl.Http.Newtonsoft;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+public static class SfuFeature
+{
+    public const string HttpClientKey = "sfu_flurl_http_client";
+
+    public static IHostApplicationBuilder AddSelectiveForwardingUnit(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<IArgonSelectiveForwardingUnit, ArgonSelectiveForwardingUnit>();
+        builder.Services.Configure<SfuFeatureSettings>(builder.Configuration.GetSection("sfu"));
+        builder.Services.AddKeyedScoped<IFlurlClient, FlurlClient>(HttpClientKey, (provider, o) =>
+        {
+            var client = new FlurlClient(provider.GetRequiredService<IOptions<SfuFeatureSettings>>().Value.Url);
+            client.Settings.JsonSerializer = new NewtonsoftJsonSerializer(new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+            return client;
+        });
+
+        return builder;
+    }
+}
+
+public class SfuFeatureSettings
+{
+    public required string Url { get; set; }
+    public required string ClientId { get; set; }
+    public required string ClientSecret { get; set; }
+}
