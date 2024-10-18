4571941a607c132497bf6ccc428583ab5593bf5f
Yuuki Wesp <ls-micro@ya.ru>
2024-10-19 02:56:46 +0300

    add sfu models

---
+++ src/Argon.Api/Features/Sfu/Models.cs
@@ -0,0 +1,17 @@
+﻿namespace Argon.Sfu;
+
+using LiveKit.Proto;
+
+public record struct EphemeralChannelInfo(ArgonChannelId channelId, string sid, Room room);
+public record struct RealtimeToken(string value);
+
+public record struct ArgonUserId(Guid id)
+{
+    public string ToRawIdentity() => id.ToString("N");
+}
+public record struct ArgonServerId(Guid id);
+
+public record struct ArgonChannelId(ArgonServerId serverId, Guid channelId)
+{
+    public string ToRawRoomId() => $"{serverId.id:N}:{channelId:N}";
+}
