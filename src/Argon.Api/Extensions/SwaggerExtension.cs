ba7139f34f106c1aaaf7e1d53dbc4aea04174367
Aram <aramhrptn@hotmail.com>
2024-10-22 02:32:55 +0400

    Add JWT, Orleans, Swagger extensions and UserManagerService

---
+++ src/Argon.Api/Extensions/SwaggerExtension.cs
@@ -0,0 +1,37 @@
+namespace Argon.Api.Extensions;
+
+using Microsoft.OpenApi.Models;
+
+public static class SwaggerExtension
+{
+    public static WebApplicationBuilder AddSwaggerWithAuthHeader(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "x-argon-token",
+                    In = ParameterLocation.Header,
+                    Description = "access token"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        []
+                    }
+                });
+            })
+            .AddEndpointsApiExplorer();
+
+        return builder;
+    }
+
+}
