e918cfe74543273b97ee86299c1bbcf37666deaf
Yuuki Wesp <ls-micro@ya.ru>
2024-10-22 08:23:26 +0300

    add fusion

---
+++ src/Argon.Api/Services/UserAuthorization.cs
@@ -0,0 +1,14 @@
+﻿namespace Argon.Api.Services;
+
+using Grains.Interfaces;
+using Contracts;
+
+public class UserAuthorization(IGrainFactory grainFactory) : IUserAuthorization
+{
+    public async Task<AuthorizeResponse> AuthorizeAsync(AuthorizeRequest request)
+    {
+        // TODO machineKey
+        var token = await grainFactory.GetGrain<IUserManager>(request.username).Authenticate(request.password);
+        return new AuthorizeResponse(token, [new ServerResponse(Guid.NewGuid(), "xuita", null)]);
+    }
+}
