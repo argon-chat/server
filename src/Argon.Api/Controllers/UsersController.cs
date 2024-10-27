125d22b0270873e174e8775a36389e44f5d2e00f
Aram <aramhrptn@hotmail.com>
2024-10-27 09:11:43 +0400

    Add server and channel endpoints to UsersController

--- src/Argon.Api/Controllers/UsersController.cs
+++ src/Argon.Api/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using Grains.Interfaces;
 using Grains.Persistence.States;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Sfu;
 
 #if DEBUG
 public record UserInputDto(string Username, string Password);
@@ -41,6 +42,52 @@ public class UsersController(IGrainFactory grainFactory, ILogger<UsersController
     {
         var userManager = grainFactory.GetGrain<IUserManager>(username);
         return await userManager.Get();
+    }
+
+    [HttpPost("server")]
+    [Authorize]
+    public async Task<ActionResult<ServerStorage>> CreateServer([FromBody] string name, string description)
+    {
+        var username = User.Claims.FirstOrDefault(cl => cl.Type == "username")?.Value;
+        var userManager = grainFactory.GetGrain<IUserManager>(username);
+        return await userManager.CreateServer(name, description);
+    }
+
+    [HttpGet("servers")]
+    [Authorize]
+    public async Task<ActionResult<List<ServerStorage>>> GetServers()
+    {
+        var username = User.Claims.FirstOrDefault(cl => cl.Type == "username")?.Value;
+        var userManager = grainFactory.GetGrain<IUserManager>(username);
+        return await userManager.GetServers();
+    }
+
+    [HttpGet("servers/{serverId}/channels")]
+    [Authorize]
+    public async Task<ActionResult<IEnumerable<ChannelStorage>>> GetServerChannels(Guid serverId)
+    {
+        var username = User.Claims.FirstOrDefault(cl => cl.Type == "username")?.Value;
+        var userManager = grainFactory.GetGrain<IUserManager>(username);
+        var channels = await userManager.GetServerChannels(serverId);
+        return channels.ToList();
+    }
+
+    [HttpGet("servers/{serverId}/channels/{channelId}")]
+    [Authorize]
+    public async Task<ActionResult<ChannelStorage>> GetChannel(Guid serverId, Guid channelId)
+    {
+        var username = User.Claims.FirstOrDefault(cl => cl.Type == "username")?.Value;
+        var userManager = grainFactory.GetGrain<IUserManager>(username);
+        return await userManager.GetChannel(serverId, channelId);
+    }
+
+    [HttpPost("servers/{serverId}/channels/{channelId}/join")]
+    [Authorize]
+    public async Task<ActionResult<RealtimeToken>> JoinChannel(Guid serverId, Guid channelId)
+    {
+        var username = User.Claims.FirstOrDefault(cl => cl.Type == "username")?.Value;
+        var userManager = grainFactory.GetGrain<IUserManager>(username);
+        return await userManager.JoinChannel(serverId, channelId);
     }
 }
 #endif
