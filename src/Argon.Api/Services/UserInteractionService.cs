f6496884331ef7f4642079a72d3025d72df64180
Aram <aramhrptn@hotmail.com>
2024-10-28 02:20:36 +0400

    Refactor UserInteractionService to reuse IUserManager instance

--- src/Argon.Api/Services/UserInteractionService.cs
+++ src/Argon.Api/Services/UserInteractionService.cs
@@ -8,26 +8,34 @@ public class UserInteractionService(
     IGrainFactory grainFactory
 ) : IUserInteraction
 {
-    public async Task<UserResponse> GetMe() => await grainFactory.GetGrain<IUserManager>(username).Get();
+    private readonly IUserManager userManager = grainFactory.GetGrain<IUserManager>(username);
 
-    public async Task<ServerResponse> CreateServer(CreateServerRequest request) =>
-        await grainFactory
-            .GetGrain<IUserManager>(username)
-            .CreateServer(request.Name, request.Description);
+    public async Task<UserResponse> GetMe()
+    {
+        return await userManager.Get();
+    }
 
-    public async Task<List<ServerResponse>> GetServers() =>
-        (await grainFactory
-            .GetGrain<IUserManager>(username)
-            .GetServers())
-        .Select(x => (ServerResponse)x)
-        .ToList();
+    public async Task<ServerResponse> CreateServer(CreateServerRequest request)
+    {
+        return await userManager.CreateServer(request.Name, request.Description);
+    }
 
-    public async Task<List<ServerDetailsResponse>> GetServerDetails(ServerDetailsRequest request) =>
-        (await grainFactory.GetGrain<IUserManager>(username).GetServerChannels(request.ServerId))
-        .Select(x => (ServerDetailsResponse)x)
-        .ToList();
+    public async Task<List<ServerResponse>> GetServers()
+    {
+        return (await userManager.GetServers())
+            .Select(x => (ServerResponse)x)
+            .ToList();
+    }
 
-    public async Task<ChannelJoinResponse> JoinChannel(ChannelJoinRequest request) =>
-        new((await grainFactory.GetGrain<IUserManager>(username)
-            .JoinChannel(request.ServerId, request.ChannelId)).value);
+    public async Task<List<ServerDetailsResponse>> GetServerDetails(ServerDetailsRequest request)
+    {
+        return (await userManager.GetServerChannels(request.ServerId))
+            .Select(x => (ServerDetailsResponse)x)
+            .ToList();
+    }
+
+    public async Task<ChannelJoinResponse> JoinChannel(ChannelJoinRequest request)
+    {
+        return new ChannelJoinResponse((await userManager.JoinChannel(request.ServerId, request.ChannelId)).value);
+    }
 }
