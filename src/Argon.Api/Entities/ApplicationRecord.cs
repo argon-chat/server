9fee420b79c0e06189e9d33cc0cf189daeae4c05
Aram <aramhrptn@hotmail.com>
2024-10-29 23:36:40 +0400

    Set default values for Id and make CreatedAt read-only

--- src/Argon.Api/Entities/ApplicationRecord.cs
+++ src/Argon.Api/Entities/ApplicationRecord.cs
@@ -2,7 +2,7 @@ namespace Argon.Api.Entities;
 
 public class ApplicationRecord
 {
-    public Guid Id { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Guid Id { get; set; } = Guid.Empty;
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
