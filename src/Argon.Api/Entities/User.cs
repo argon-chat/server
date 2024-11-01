1011ce13c69e6381e961ea5768756d3e7d521b5b
Aram <aramhrptn@hotmail.com>
2024-11-01 21:15:02 +0400

    Add one-time password (OTP) field to User entity

--- src/Argon.Api/Entities/User.cs
+++ src/Argon.Api/Entities/User.cs
@@ -5,16 +5,18 @@ using Grains.Persistence.States;
 
 public sealed class User : ApplicationRecord
 {
-    [MaxLength(255)] [MinLength(12)] public string? Email { get; set; } = string.Empty;
-
     [Required]
     [MaxLength(255)]
-    [MinLength(6)]
-    public string Username { get; set; } = string.Empty;
+    [MinLength(12)]
+    public string? Email { get; set; } = string.Empty;
+
+    [MaxLength(255)] [MinLength(6)] public string Username { get; set; } = string.Empty;
 
     [MaxLength(30)] public string? PhoneNumber { get; set; } = string.Empty;
-    [Required] [MaxLength(511)] public string PasswordDigest { get; set; } = string.Empty;
+    [MaxLength(511)] public string PasswordDigest { get; set; } = string.Empty;
     [MaxLength(1023)] public string? AvatarUrl { get; set; } = string.Empty;
+    [MaxLength(7)] public string OTP { get; set; } = string.Empty;
+
 
     public static implicit operator UserStorageDto(User user)
     {
