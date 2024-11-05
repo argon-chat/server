72c579c0854d9e27c9d97b5029304abac2ca9ccd
Aram <aramhrptn@hotmail.com>
2024-11-05 20:03:40 +0400

    Add logging for email sending in DEBUG mode

--- src/Argon.Api/Grains/EmailManager.cs
+++ src/Argon.Api/Grains/EmailManager.cs
@@ -5,7 +5,7 @@ using System.Net.Mail;
 using Interfaces;
 using Microsoft.Extensions.Options;
 
-public class EmailManager(IOptions<SmtpConfig> config) : Grain, IEmailManager
+public class EmailManager(IOptions<SmtpConfig> config, ILogger<EmailManager> logger) : Grain, IEmailManager
 {
     private SmtpClient Client => new()
     {
@@ -19,10 +19,15 @@ public class EmailManager(IOptions<SmtpConfig> config) : Grain, IEmailManager
 
     public Task SendEmailAsync(string email, string subject, string message, string template = "none")
     {
+    #if DEBUG
+        logger.LogInformation($"Email sent to {email} with subject {subject} and message {message}");
+        return Task.CompletedTask;
+    #else
         var mail = new MailMessage(config.Value.User, email, subject, message)
         {
             IsBodyHtml = true
         };
         return Client.SendMailAsync(mail);
+    #endif
     }
 }
