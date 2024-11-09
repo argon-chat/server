4bc21a6cabb6117035d0afc5059ead8ce01a5b3a
Aram <aramhrptn@hotmail.com>
2024-11-09 23:43:29 +0400

    Add activation repartitioner for Orleans

--- src/Argon.Api/Extensions/OrleansExtension.cs
+++ src/Argon.Api/Extensions/OrleansExtension.cs
@@ -1,7 +1,9 @@
 namespace Argon.Api.Extensions;
 
+using System.Diagnostics.CodeAnalysis;
 using Orleans.Clustering.Kubernetes;
 using Orleans.Configuration;
+using Orleans.Placement.Repartitioning;
 using Orleans.Storage;
 
 internal class MemoryPackStorageSerializer : IGrainStorageSerializer
@@ -11,8 +13,14 @@ internal class MemoryPackStorageSerializer : IGrainStorageSerializer
     public T Deserialize<T>(BinaryData input) => MemoryPackSerializer.Deserialize<T>(input) ?? throw new InvalidOperationException();
 }
 
+internal class Zalupus : IImbalanceToleranceRule
+{
+    public bool IsSatisfiedBy(uint imbalance) => imbalance % 2 == 0;
+}
+
 public static class OrleansExtension
 {
+    [Experimental("ORLEANSEXP001")]
     public static WebApplicationBuilder AddOrleans(this WebApplicationBuilder builder)
     {
         builder.Host.UseOrleans(siloBuilder =>
@@ -26,7 +34,7 @@ public static class OrleansExtension
                 options.Invariant              = "Npgsql";
                 options.ConnectionString       = builder.Configuration.GetConnectionString("DefaultConnection");
                 options.GrainStorageSerializer = new MemoryPackStorageSerializer();
-            }).AddReminders().AddMemoryGrainStorage("CacheStorage").UseDashboard(o => o.Port = 22832);
+            }).AddReminders().AddActivationRepartitioner<Zalupus>().AddMemoryGrainStorage("CacheStorage").UseDashboard(o => o.Port = 22832);
             if (builder.Environment.IsDevelopment()) siloBuilder.UseLocalhostClustering();
             else siloBuilder.UseKubeMembership();
         });
