f166da0d3b7d34d46786d332bf2eaf6a13f262ce
Yuuki Wesp <ls-micro@ya.ru>
2024-11-16 03:44:53 +0300

    change metadata controller

--- src/Argon.Api/Controllers/MetadataController.cs
+++ src/Argon.Api/Controllers/MetadataController.cs
@@ -1,5 +1,6 @@
 namespace Argon.Api.Controllers;
 
+using Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,24 +8,19 @@ using Microsoft.AspNetCore.Mvc;
 public class MetadataController : ControllerBase
 {
     [HttpGet("/cfg.json"), AllowAnonymous]
-    public ValueTask<HeadRoutingConfig> GetHead() => new(new HeadRoutingConfig($"{GlobalVersion.FullSemVer}.{GlobalVersion.ShortSha}",
-        "api.argon.gl", "argon-f14ic5ia.livekit.cloud", [
-            new RegionalNode("cdn-ru1.argon.gl", "ru1"),
-            new RegionalNode("cdn-ru2.argon.gl", "ru1"),
-            new RegionalNode("cdn-as1.argon.gl", "as1")
-        ], [
-            new FeatureFlag("dev.window", true),
-            new FeatureFlag("user.allowServerCreation", true)
-        ]));
+    public ValueTask<HeadRoutingConfig> GetHead()
+        => new(new HeadRoutingConfig($"{GlobalVersion.FullSemVer}.{GlobalVersion.ShortSha}",
+            "api.argon.gl", "argon-f14ic5ia.livekit.cloud", [
+                new FeatureFlag("dev.window", true),
+                new FeatureFlag("user.allowServerCreation", true)
+            ], this.HttpContext.GetRegion()));
 }
 
 public record HeadRoutingConfig(
     string version,
     string masterEndpoint,
     string webRtcEndpoint,
-    List<RegionalNode> cdnAddresses,
-    List<FeatureFlag> features);
-
-public record RegionalNode(string url, string code);
+    List<FeatureFlag> features,
+    string currentRegionCode);
 
 public record FeatureFlag(string code, bool enabled);
