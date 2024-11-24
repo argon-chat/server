0a7006d89f3f273780d04136b4ed3c9edc0f7e93
Yuuki Wesp <ls-micro@ya.ru>
2024-11-24 08:34:25 +0300

    add sentry filter for orleans, add sentry open telemetry

--- src/Argon.Api/Features/Orleanse/OrleansExtension.cs
+++ src/Argon.Api/Features/Orleanse/OrleansExtension.cs
@@ -1,6 +1,7 @@
 namespace Argon.Api.Features;
 
 using ActualLab.Serialization;
+using Argon.Api.Features.Sentry;
 using Contracts;
 using Env;
 using Extensions;
@@ -17,27 +18,38 @@ public static class OrleansExtension
     public static WebApplicationBuilder AddOrleans(this WebApplicationBuilder builder)
     {
         builder.Services.AddSerializer(x => x.AddMessagePackSerializer(null, null, MessagePackByteSerializer.Default.Options));
-        builder.Host.UseOrleans(siloBuilder => {
-            siloBuilder.Configure<ClusterOptions>(builder.Configuration.GetSection("Orleans")).AddStreaming().UseDashboard(o => o.Port = 22832)
-               .AddActivityPropagation().AddAdoNetGrainStorage("PubSubStore", options =>
+        builder.Host.UseOrleans(siloBuilder =>
+        {
+            siloBuilder.Configure<ClusterOptions>(builder.Configuration.GetSection("Orleans"))
+               .AddStreaming()
+               .UseDashboard(o => o.Port = 22832)
+               .AddActivityPropagation()
+               .AddIncomingGrainCallFilter<SentryGrainCallFilter>()
+               .AddAdoNetGrainStorage("PubSubStore", options =>
                 {
-                    options.Invariant              = "Npgsql";
-                    options.ConnectionString       = builder.Configuration.GetConnectionString("DefaultConnection");
+                    options.Invariant        = "Npgsql";
+                    options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
                 }).AddAdoNetGrainStorage("OrleansStorage", options =>
                 {
-                    options.Invariant              = "Npgsql";
-                    options.ConnectionString       = builder.Configuration.GetConnectionString("DefaultConnection");
+                    options.Invariant        = "Npgsql";
+                    options.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
                 });
 
             if (builder.Environment.IsKube())
             {
-                siloBuilder.UseKubeMembership().AddActivationRepartitioner<BalanceRule>().AddRedisStorage(IFusionSessionGrain.StorageId, 2)
+                siloBuilder
+                   .UseKubeMembership()
+                   .AddActivationRepartitioner<BalanceRule>()
+                   .AddRedisStorage(IFusionSessionGrain.StorageId, 2)
                    .AddPersistentStreams("default", NatsAdapterFactory.Create, options => { })
                    .AddPersistentStreams(IArgonEvent.ProviderId, NatsAdapterFactory.Create, options => { });
             }
             else
             {
-                siloBuilder.UseLocalhostClustering().AddMemoryStreams("default").AddMemoryStreams(IArgonEvent.ProviderId)
+                siloBuilder
+                   .UseLocalhostClustering()
+                   .AddMemoryStreams("default")
+                   .AddMemoryStreams(IArgonEvent.ProviderId)
                    .AddMemoryGrainStorage(IFusionSessionGrain.StorageId);
             }
         });
