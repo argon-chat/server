f7fd665534dc321ae053b28a0bffebb84c1e0c08
Yuuki Wesp <ls-micro@ya.ru>
2024-11-24 09:09:12 +0300

    Improve logging and error handling
    - Added logging for file uploads to S3, including success and failure messages.
    - Changed error logs to warnings for user not found and registration issues.
    - Enhanced password hashing service with critical logging on hash computation failures.

--- src/Argon.Api/Features/MediaStorage/Storages/S3ContentStorage.cs
+++ src/Argon.Api/Features/MediaStorage/Storages/S3ContentStorage.cs
@@ -1,9 +1,11 @@
 namespace Argon.Api.Features.MediaStorage.Storages;
 
 using Genbox.SimpleS3.Core.Abstracts.Clients;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-public class S3ContentStorage([FromKeyedServices("GenericS3:client")] IObjectClient s3Client, IOptions<StorageOptions> options) : IContentStorage
+public class S3ContentStorage([FromKeyedServices("GenericS3:client")] IObjectClient s3Client, IOptions<StorageOptions> options, 
+    ILogger<IContentStorage> logger) : IContentStorage
 {
     public ValueTask<StorageSpace> GetStorageStats()
         => new(new StorageSpace(0, 0, 0));
@@ -11,13 +13,19 @@ public class S3ContentStorage([FromKeyedServices("GenericS3:client")] IObjectCli
     public async ValueTask UploadFile(StorageNameSpace block, AssetId assetId, Stream data)
     {
         var config = options.Value;
+        logger.LogInformation("Begin upload file to s3 storage, '{bucketName}' to '{path}'", 
+            config.BucketName, $"{block.ToPath()}/{assetId.GetFilePath()}");
         var result = await s3Client.PutObjectAsync(config.BucketName, $"{block.ToPath()}/{assetId.GetFilePath()}", data, request => {
             foreach (var (key, value) in assetId.GetTags(block))
                 request.Tags.Add(key, value);
         });
 
         if (!result.IsSuccess)
+        {
+            logger.LogCritical("Failed upload file to s3 storage, '{bucketName}' to '{path}', errorCode: {errorCode}, errorMessage: {errorMessage}",
+                config.BucketName, $"{block.ToPath()}/{assetId.GetFilePath()}", result.Error?.Code, result.Error?.Message);
             throw new InvalidOperationException();
+        }
     }
 
     public async ValueTask DeleteFile(StorageNameSpace block, AssetId assetId)
