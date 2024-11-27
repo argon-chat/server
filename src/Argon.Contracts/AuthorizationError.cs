03354f929fd6bf6788974e3d91912ced6dd0d674
Yuuki Wesp <ls-micro@ya.ru>
2024-11-28 00:43:27 +0300

    add lockdown operatioln

--- src/Argon.Contracts/AuthorizationError.cs
+++ src/Argon.Contracts/AuthorizationError.cs
@@ -21,3 +21,18 @@ public enum RegistrationError
     SSO_EMAILS_NOT_ALLOWED,
     INTERNAL_ERROR
 }
+
+
+[TsEnum]
+public enum LockdownReason
+{
+    NONE = 0,
+    SPAM_SCAM_ACCOUNT,
+    INCITING_MOMENT,
+    NON_BINARY_PERSON,
+    TOS_VIOLATION,
+    LGBT_AGITATION,
+    DRUG_VIOLATION,
+    TERRORISM_AGITATION,
+    CHILD_ABUSE
+}
