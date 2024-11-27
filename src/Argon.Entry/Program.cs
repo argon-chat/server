02942a08e363eec27fec84339418a436a059dd02
Aram <aramhrptn@hotmail.com>
2024-11-28 01:33:10 +0400

    Add Sentry and refactor service registrations
    Integrated Sentry for application monitoring by adding its configuration. Refactored the service registration code to improve readability, removing redundant imports and consolidating chained method calls.

--- src/Argon.Entry/Program.cs
+++ src/Argon.Entry/Program.cs
@@ -4,7 +4,6 @@ using ActualLab.Rpc.Server;
 using ActualLab.Serialization;
 using Argon.Api;
 using Argon.Api.Controllers;
-using Argon.Api.Entities;
 using Argon.Api.Extensions;
 using Argon.Api.Features.Jwt;
 using Argon.Api.Features.Pex;
@@ -16,7 +15,7 @@ using Orleans.Configuration;
 using Orleans.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.AddSentry(builder.Configuration.GetConnectionString("Sentry"));
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.Limits.KeepAliveTimeout                  = TimeSpan.FromSeconds(400);
@@ -30,8 +29,7 @@ builder.WebHost.ConfigureKestrel(options =>
 builder.AddArgonPermissions();
 builder.AddServiceDefaults();
 builder.AddJwt();
-builder.Services.AddControllers()
-   .AddApplicationPart(typeof(AuthorizationController).Assembly)
+builder.Services.AddControllers().AddApplicationPart(typeof(AuthorizationController).Assembly)
    .AddNewtonsoftJson(x => x.SerializerSettings.Converters.Add(new StringEnumConverter()));
 builder.Services.AddCors(x =>
 {
@@ -42,22 +40,18 @@ builder.Services.AddCors(x =>
         z.AllowAnyMethod();
     });
 });
-builder.Services.AddFusion(RpcServiceMode.Server, true)
-   .Rpc.AddWebSocketServer(true).Rpc
-   .AddServer<IUserInteraction, UserInteraction>()
-   .AddServer<IServerInteraction, ServerInteraction>()
-   .AddServer<IEventBus, EventBusService>()
+builder.Services.AddFusion(RpcServiceMode.Server, true).Rpc.AddWebSocketServer(true).Rpc.AddServer<IUserInteraction, UserInteraction>()
+   .AddServer<IServerInteraction, ServerInteraction>().AddServer<IEventBus, EventBusService>()
    .AddServer<IUserPreferenceInteraction, UserPreferenceInteraction>();
 builder.AddSwaggerWithAuthHeader();
-builder.Services.AddSerializer(x => x.AddMessagePackSerializer(null, null, MessagePackByteSerializer.Default.Options))
-   .AddOrleansClient(x =>
-    {
-        x.Configure<ClusterOptions>(builder.Configuration.GetSection("Orleans")).AddStreaming();
-        if (builder.Environment.IsProduction())
-            x.UseKubeGatewayListProvider();
-        else
-            x.UseLocalhostClustering();
-    });
+builder.Services.AddSerializer(x => x.AddMessagePackSerializer(null, null, MessagePackByteSerializer.Default.Options)).AddOrleansClient(x =>
+{
+    x.Configure<ClusterOptions>(builder.Configuration.GetSection("Orleans")).AddStreaming();
+    if (builder.Environment.IsProduction())
+        x.UseKubeGatewayListProvider();
+    else
+        x.UseLocalhostClustering();
+});
 builder.Services.AddAuthorization();
 builder.Services.AddSingleton<IFusionContext, FusionContext>();
 var app = builder.Build();
