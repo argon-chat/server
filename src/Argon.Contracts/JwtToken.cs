8c686cc0e5a32229873add94a0df10a759b4b60c
Yuuki Wesp <ls-micro@ya.ru>
2024-12-04 02:25:38 +0300

    drop unused code

--- src/Argon.Contracts/JwtToken.cs
+++
@@ -1,9 +0,0 @@
-namespace Argon;
-
-using Argon.Contracts;
-using MemoryPack;
-using Orleans;
-using Reinforced.Typings.Attributes;
-
-[Serializable, GenerateSerializer, Alias(nameof(JwtToken)), TsInterface]
-public record JwtToken([field: Id(0)] string token);
--- src/Argon.Contracts/Models/ArchetypeModel/IArchetypeObject.cs
+++
@@ -1,9 +0,0 @@
-namespace Argon.Contracts.Models.ArchetypeModel;
-
-using Reinforced.Typings.Attributes;
-
-[TsInterface]
-public interface IArchetypeObject
-{
-    ICollection<IArchetypeOverwrite> Overwrites { get; }
-}
