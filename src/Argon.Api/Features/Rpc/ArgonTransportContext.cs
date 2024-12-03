0df3e60f30227d1917b04aeb919193560ef6fd93
Yuuki Wesp <ls-micro@ya.ru>
2024-12-04 02:38:05 +0300

    Add Argon RPC features
    descriptor storage, extensions, transport, context, auth interceptor. Add service registration extensions for RPC services.

---
+++ src/Argon.Api/Features/Rpc/ArgonTransportContext.cs
@@ -0,0 +1,42 @@
+namespace Argon.Services;
+
+using Features.Jwt;
+using Grpc.Core;
+using Transport;
+
+public class ArgonTransportContext(ServerCallContext RpcContext, RpcRequest Request, IServiceProvider provider) : IDisposable
+{
+    private static readonly AsyncLocal<ArgonTransportContext> localScope = new();
+
+    public string GetIpAddress()
+        => RpcContext.GetHttpContext().GetIpAddress();
+
+    public string GetRegion()
+        => RpcContext.GetHttpContext().GetRegion();
+
+    public string GetRay()
+        => RpcContext.GetHttpContext().GetRay();
+
+    public string GetClientName()
+        => RpcContext.GetHttpContext().GetClientName();
+
+    public string GetHostName()
+        => RpcContext.GetHttpContext().GetHostName();
+
+    public static ArgonTransportContext Current
+        => localScope.Value ?? throw new InvalidOperationException($"No active transport context");
+
+    public static ArgonTransportContext Create(ServerCallContext ctx, RpcRequest request, IServiceProvider provider)
+    {
+        if (localScope.Value is not null)
+            throw new InvalidAsynchronousStateException($"AsyncLocal of ArgonTransportContext already active");
+        return localScope.Value = new ArgonTransportContext(ctx, request, provider);
+    }
+
+    public bool IsAuthorized => RpcContext.UserState.ContainsKey("userToken");
+
+    public TokenUserData User => RpcContext.UserState["userToken"] as TokenUserData ?? throw new InvalidOperationException();
+
+    public void Dispose()
+        => localScope.Value = null!;
+}
