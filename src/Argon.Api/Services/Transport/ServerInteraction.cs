69482441c7f48a3acbeeee3a3d0930492ce4cda9
Yuuki Wesp <ls-micro@ya.ru>
2024-12-04 05:03:48 +0300

    Refactor server interaction methods for better clarity
    - Updated method signatures for CreateChannel, DeleteChannel, JoinToVoiceChannel
    - Added GetChannels and GetMembers methods to retrieve server data

--- src/Argon.Api/Services/Transport/ServerInteraction.cs
+++ src/Argon.Api/Services/Transport/ServerInteraction.cs
@@ -2,24 +2,30 @@ namespace Argon.Services;
 
 public class ServerInteraction(IGrainFactory grainFactory) : IServerInteraction
 {
-    public async Task<CreateChannelResponse> CreateChannel(CreateChannelRequest request)
-    {
-        var user = this.GetUser();
-        var result = await grainFactory
+    public async Task CreateChannel(CreateChannelRequest request)
+        => await grainFactory
            .GetGrain<IServerGrain>(request.serverId)
-           .CreateChannel(new ChannelInput(request.name, new ChannelEntitlementOverwrite(), request.desc, request.kind), user.id);
-        return new CreateChannelResponse(request.serverId, result.Id);
-    }
+           .CreateChannel(new ChannelInput(request.name, new ChannelEntitlementOverwrite(), request.desc, request.kind), this.GetUser().id);
 
-    public Task DeleteChannel(DeleteChannelRequest request)
+    public Task DeleteChannel(Guid serverId, Guid channelId)
         => throw new NotImplementedException();
 
-    public async Task<ChannelJoinResponse> JoinToVoiceChannel(JoinToVoiceChannelRequest request)
+    public async Task<string> JoinToVoiceChannel(Guid serverId, Guid channelId)
     {
         var user = this.GetUser();
         var result = await grainFactory
-           .GetGrain<IChannelGrain>(request.channelId)
+           .GetGrain<IChannelGrain>(channelId)
            .Join(user.id);
-        return new ChannelJoinResponse(result.Value.value);
+        return result.Value.value;
     }
+
+    public Task<List<RealtimeChannel>> GetChannels(Guid serverId)
+        => grainFactory
+           .GetGrain<IServerGrain>(serverId)
+           .GetChannels();
+
+    public Task<List<RealtimeServerMember>> GetMembers(Guid serverId)
+        => grainFactory
+           .GetGrain<IServerGrain>(serverId)
+           .GetMembers();
 }
--- src/Argon.Contracts/IServerInteraction.cs
+++ src/Argon.Contracts/IServerInteraction.cs
@@ -5,7 +5,11 @@ using Streaming.Events;
 [TsInterface]
 public interface IServerInteraction : IArgonService
 {
-    Task<CreateChannelResponse> CreateChannel(CreateChannelRequest request);
-    Task                        DeleteChannel(DeleteChannelRequest request);
-    Task<ChannelJoinResponse>   JoinToVoiceChannel(JoinToVoiceChannelRequest request);
+    Task         CreateChannel(CreateChannelRequest request);
+    Task         DeleteChannel(Guid serverId, Guid channelId);
+    Task<string> JoinToVoiceChannel(Guid serverId, Guid channelId);
+
+
+    Task<List<RealtimeChannel>>      GetChannels(Guid serverId);
+    Task<List<RealtimeServerMember>> GetMembers(Guid serverId);
 }
