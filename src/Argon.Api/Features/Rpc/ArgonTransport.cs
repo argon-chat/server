3b8bf46892f3c78a4dade709d869b8fe29a0db56
Yuuki Wesp <ls-micro@ya.ru>
2025-01-25 01:40:03 +0300

    Refactor RPC methods, add context creation for Unary requests.
    The commit refactors the RPC methods and adds context creation specifically for Unary requests. The BiDirectSubscribe and BroadcastSubscribe methods have been removed, while a new context creation method is introduced for Unary requests.

--- src/Argon.Api/Features/Rpc/ArgonTransport.cs
+++ src/Argon.Api/Features/Rpc/ArgonTransport.cs
@@ -8,93 +8,12 @@ using Transport;
 public class ArgonTransport(IServiceProvider provider, ArgonDescriptorStorage storage, ILogger<ArgonTransport> logger)
     : Transport.ArgonTransport.ArgonTransportBase
 {
-    public async override Task BiDirectSubscribe(IAsyncStreamReader<RpcRequest> requestStream, IServerStreamWriter<StreamPayload> responseStream,
-        ServerCallContext context)
-    {
-        while (await requestStream.MoveNext())
-        {
-            await responseStream.WriteAsync(new StreamPayload()
-            {
-                Payload = requestStream.Current.Payload
-            });
-        }
-    }
-
-    public async override Task BroadcastSubscribe(RpcRequest request, IServerStreamWriter<StreamPayload> responseStream, ServerCallContext context)
-    {
-        using var scope = ArgonTransportContext.Create(context, request, provider);
-
-        var service = storage.GetService(request.Interface);
-
-        try
-        {
-            var method = service.GetType().GetMethod(request.Method);
-            if (method == null)
-                throw new InvalidOperationException($"Method '{request.Method}' not found in service '{service.GetType().Name}'.");
-
-            if (method.GetCustomAttribute<AllowAnonymousAttribute>() is null && !scope.IsAuthorized)
-                throw new UnauthorizedAccessException($"Not authorized to access method '{request.Method}'.");
-
-            var parameters     = method.GetParameters();
-            var parameterTypes = parameters.Select(p => p.ParameterType).ToArray();
-
-            var arguments = MessagePackSerializer.Deserialize<object[]>(request.Payload.Memory);
-
-            if (arguments.Length != parameterTypes.Length)
-                throw new InvalidOperationException(
-                    $"Method '{method.Name}' expects {parameterTypes.Length} arguments, but received {arguments.Length}."
-                );
-
-            var typedArguments = arguments
-               .Zip(parameterTypes, (arg, type) => MessagePackSerializer.Deserialize(type, MessagePackSerializer.Serialize(arg))).ToArray();
-
-            var result = method.Invoke(service, typedArguments);
-            if (result is not Task<IArgonStream<IArgonEvent>> resultTask)
-                throw new InvalidOperationException($"Method '{request.Method}' must return IArgonStream<IArgonEvent>.");
-
-            var argonStream = await resultTask;
-
-            await using var stream = argonStream;
-
-            IAsyncEnumerable<IArgonEvent> enumerator = stream;
-
-            await foreach (var argonEvent in enumerator.ConfigureAwait(false).WithCancellation(context.CancellationToken))
-            {
-                if (context.CancellationToken.IsCancellationRequested)
-                    return;
-
-                var payload = MessagePackSerializer.Serialize(argonEvent, cancellationToken: context.CancellationToken);
-
-                try
-                {
-                    await responseStream.WriteAsync(new StreamPayload
-                    {
-                        Payload = ByteString.CopyFrom(payload),
-                    }, context.CancellationToken).ConfigureAwait(false);
-                }
-                catch (Exception ex)
-                {
-                    logger.LogCritical(ex, "Failed to execute write to stream");
-                }
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            // ignore cancel 
-        }
-        catch (Exception e)
-        {
-            logger.LogError(e, "Failed to execute broadcast subscription");
-        }
-    }
-
-
     public async override Task<RpcResponse> Unary(RpcRequest request, ServerCallContext context)
     {
         await using var scope = provider.CreateAsyncScope();
         using var _ = scope.ServiceProvider.GetRequiredService<IServerTimingRecorder>()
            .BeginRecord($"Unary/{request.Interface}::{request.Method}");
-        using var ctx     = ArgonTransportContext.Create(context, request, provider);
+        using var ctx     = ArgonTransportContext.CreateGrpc(context, provider);
         var       service = storage.GetService(request.Interface);  
 
         try
