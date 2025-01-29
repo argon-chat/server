5116640468470cfb43f48ac7ee8c401992bfd9dd
Yuuki Wesp <ls-micro@ya.ru>
2025-01-29 11:40:17 +0300

    Add Newtonsoft.Json.JObject for webhook event handling
    - Added a using directive for Newtonsoft.Json.Linq to handle webhook events more effectively in the Webhook method of LiveKitWebHookController. Updated logging message to include JSON representation of the webhook event.

--- src/Argon.Api/Controllers/LiveKitWebHookController.cs
+++ src/Argon.Api/Controllers/LiveKitWebHookController.cs
@@ -1,15 +1,15 @@
 namespace Argon.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 [ApiController, ApiExplorerSettings(IgnoreApi = true)]
 public class LiveKitWebHookController(ILogger<LiveKitWebHookController> logger) : ControllerBase
 {
     [HttpPost("/webhook-endpoint")]
-    public IActionResult Webhook([FromBody] LiveKit.Proto.WebhookEvent webhookEvent)
+    public IActionResult Webhook([FromBody] JToken webhookEvent)
     {
-        logger.LogInformation("Called livekit webhook, event: {webhookEvent}, id: {webhookEventId}, {source}, {auth}",
-            webhookEvent.Event, webhookEvent.Id, webhookEvent.ToString(), this.Request.Headers.Authorization.ToString());
+        logger.LogInformation("Called livekit webhook, event: {json}", webhookEvent.ToString());
         return Ok();
     }
 }
