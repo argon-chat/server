77478186aebb3a2b6a3ca19daaea5b8a76277793
Yuuki Wesp <ls-micro@ya.ru>
2025-03-13 19:19:26 +0300

    Refactor environment and role determination methods
    Added methods to determine different environment types and roles based on specified conditions. Introduced new role kinds for better classification.

--- src/Argon.Api/Features/Env/EnvironmentExtensions.cs
+++ src/Argon.Api/Features/Env/EnvironmentExtensions.cs
@@ -1,38 +1,70 @@
 namespace Argon.Features.Env;
 
-using static File;
+using System.Diagnostics;
 
 public static class EnvironmentExtensions
 {
-    public static bool IsKube(this IHostEnvironment env)
-        => env.Determine() == ArgonEnvironmentKind.Kubernetes;
+    public static bool IsMultiRegion(this IHostEnvironment env)
+        => env.Determine() == ArgonEnvironmentKind.MultiRegion;
+    public static bool IsSingleRegion(this IHostEnvironment env)
+        => env.Determine() == ArgonEnvironmentKind.SingleRegion;
+    public static bool IsSingleInstance(this IHostEnvironment env)
+        => env.Determine() == ArgonEnvironmentKind.SingleInstance;
 
-    public static bool IsDocker(this IHostEnvironment env)
-        => env.Determine() == ArgonEnvironmentKind.Docker;
-
-    public static bool IsClassicHost(this IHostEnvironment env)
-        => env.Determine() == ArgonEnvironmentKind.HostMachine;
-
-    public static bool IsManaged(this IHostEnvironment env)
-        => env.IsDocker() || env.IsKube();
+    public static bool IsRegionalMode(this IHostEnvironment env)
+        => env.IsSingleInstance() || env.IsMultiRegion();
 
     public static ArgonEnvironmentKind Determine(this IHostEnvironment _)
     {
-        if (Environment.GetEnvironmentVariable("ENV_PROC_OVERRIDE") is { } newEnv)
-            return Enum.Parse<ArgonEnvironmentKind>(newEnv);
+        if (Debugger.IsAttached)
+            return ArgonEnvironmentKind.SingleInstance;
 
-        if (Environment.GetEnvironmentVariable("KUBERNETES_SERVICE_HOST") != null)
-            return ArgonEnvironmentKind.Kubernetes;
-        if (Exists("/.dockerenv") || Directory.Exists("/proc/self/cgroup") &&
-            ReadAllText("/proc/self/cgroup").Contains("docker"))
-            return ArgonEnvironmentKind.Docker;
-        return ArgonEnvironmentKind.HostMachine;
+        if (Environment.GetEnvironmentVariable("ARGON_MODE") is { } newEnv)
+            return Enum.Parse<ArgonEnvironmentKind>(newEnv);
+        throw new InvalidOperationException("No defined 'ARGON_MODE' environment variable, no defined argon mode");
     }
 }
 
 public enum ArgonEnvironmentKind
 {
-    HostMachine,
-    Docker,
-    Kubernetes,
+    SingleInstance,
+    SingleRegion,
+    MultiRegion
+}
+
+
+public static class EnvironmentRoleExtensions
+{
+    public static bool IsGateway(this IHostEnvironment env)
+        => env.DetermineRole() == ArgonRoleKind.Gateway;
+    public static bool IsEntryPoint(this IHostEnvironment env)
+        => env.DetermineRole() == ArgonRoleKind.EntryPoint;
+    public static bool IsWorker(this IHostEnvironment env)
+        => env.DetermineRole() == ArgonRoleKind.Worker;
+    public static bool IsHybrid(this IHostEnvironment env)
+        => env.DetermineRole() == ArgonRoleKind.Hybrid;
+
+    public static bool IsGatewayRole(this WebApplicationBuilder env)
+        => env.Environment.DetermineRole() == ArgonRoleKind.Gateway;
+    public static bool IsEntryPointRole(this WebApplicationBuilder env)
+        => env.Environment.DetermineRole() == ArgonRoleKind.EntryPoint;
+    public static bool IsWorkerRole(this WebApplicationBuilder env)
+        => env.Environment.DetermineRole() == ArgonRoleKind.Worker;
+    public static bool IsHybridRole(this WebApplicationBuilder env)
+        => env.Environment.DetermineRole() == ArgonRoleKind.Hybrid;
+
+    public static ArgonRoleKind DetermineRole(this IHostEnvironment _)
+    {
+        if (Environment.GetEnvironmentVariable("ARGON_ROLE") is { } newEnv)
+            return Enum.Parse<ArgonRoleKind>(newEnv);
+        throw new InvalidOperationException("No defined 'ARGON_ROLE' environment variable, no defined argon role");
+    }
+}
+
+public enum ArgonRoleKind
+{
+    Hybrid,
+    Gateway,
+    EntryPoint,
+    Worker
 }
