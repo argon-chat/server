da8a6dc8a6626b141080db713728ef710d7abe7a
Yuuki Wesp <ls-micro@ya.ru>
2025-03-23 02:44:40 +0300

    Add DcClusterConnectionListener for monitoring cluster status
    - Added a new class DcClusterConnectionListener to monitor and handle cluster connection status changes. The class includes methods to notify when the gateway count changes and when the cluster connection is lost, updating the data center status accordingly.

---
+++ src/Argon.Api/Features/Orleanse/Client/DcClusterConnectionListener.cs
@@ -0,0 +1,45 @@
+namespace Argon.Api.Features.Orleans.Client;
+
+using Argon.Features;
+
+
+public enum ArgonDataCenterStatus
+{
+    ADDED,
+    OFFLINE,
+    ONLINE,
+    MAINTENANCE
+}
+
+
+public class DcClusterConnectionListener(ILogger<DcClusterConnectionListener> logger, IArgonDcRegistry registry, [FromKeyedServices("dc")] string dc) : IClusterConnectionStatusObserver
+{
+    public void NotifyGatewayCountChanged(int currentNumberOfGateways, int previousNumberOfGateways, bool connectionRecovered)
+    {
+        logger.LogInformation("NotifyGatewayCountChanged {currentNumberOfGateways}, {previousNumberOfGateways}, {connectionRecovered}",
+            currentNumberOfGateways, previousNumberOfGateways, connectionRecovered);
+
+        if (currentNumberOfGateways == 0)
+            return;
+        if (!connectionRecovered)
+            return;
+
+        if (!registry.TryGet(dc, out var self))
+            throw new NotSupportedException($"Datacenter {dc} not found in cluster registry");
+        registry.Upsert(self with
+        {
+            status = ArgonDataCenterStatus.ONLINE
+        });
+    }
+
+    public void NotifyClusterConnectionLost()
+    {
+        if (!registry.TryGet(dc, out var self))
+            throw new NotSupportedException($"Datacenter {dc} not found in cluster registry");
+        logger.LogWarning("NotifyClusterConnectionLost {dc} lost connection, going to offline", self.dc);
+        registry.Upsert(self with
+        {
+            status = ArgonDataCenterStatus.OFFLINE
+        });
+    }
+}
