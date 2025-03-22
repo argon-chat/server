fb30fb07b7d5991bc6280c8b2a0be8e600a65409
Yuuki Wesp <ls-micro@ya.ru>
2025-03-23 02:45:09 +0300

    Add DcWatcherService for monitoring data centers
    - Added a new service to monitor data center status and perform cleanup tasks based on timeout.

---
+++ src/Argon.Api/Features/Orleanse/DcWatcherService.cs
@@ -0,0 +1,55 @@
+namespace Argon.Features;
+
+using RegionalUnit;
+using Systems;
+
+public sealed class DcWatcherService(
+    ILogger<DcWatcherService> logger,
+    IArgonRegionalBus bus,
+    IArgonDcRegistry registry,
+    IArgonClusterRouter router,
+    IClusterClientFactory clusterClientFactory) : RecurringWorkerService<DcWatcherService>(logger)
+{
+    private readonly TimeSpan _gcTimeout = TimeSpan.FromMinutes(15);
+
+    protected override Task OnCreateAsync(CancellationToken ct = default)
+        => registry.SubscribeToNewClient(OnClusterRegistered);
+
+    private async ValueTask OnClusterRegistered(ArgonDcClusterInfo dc, CancellationToken ct)
+        => await dc.serviceProvider
+           .GetRequiredService<IClusterClient>()
+           .As<IClusterClient, IHostedService>()
+           .StartAsync(dc.ctSource.Token);
+
+    protected async override Task RunAsync(CancellationToken ct = default)
+    {
+        var dcs = await bus.GetAllDcsAsync(ct);
+        foreach (var dc in dcs)
+        {
+            if (!registry.TryGet(dc, out var existing))
+            {
+                var eff = await router.ComputeEffectivity(dc);
+                var sp  = await clusterClientFactory.CreateClusterClient(dc, ct);
+                registry.Upsert(new(dc, eff, sp, DateTime.Now, ArgonDataCenterStatus.ADDED, new()));
+                logger.LogInformation("DC [{dc}] added with effectivity {effectivity}", dc, eff);
+            }
+            else
+                registry.Upsert(existing with
+                {
+                    lastSeen = DateTime.UtcNow
+                });
+        }
+
+        foreach (var kv in registry.GetAll()
+                    .Where(x => x.Value.status is ArgonDataCenterStatus.OFFLINE)
+                    .Where(x => DateTime.UtcNow - x.Value.lastSeen > _gcTimeout))
+        {
+            logger.LogWarning("DC [{dc}] removed after stale timeout", kv.Key);
+            await kv.Value.ctSource.CancelAsync();
+            (kv.Value.serviceProvider as IDisposable)?.Dispose();
+            registry.Remove(kv.Key);
+        }
+
+        await Task.Delay(TimeSpan.FromSeconds(30), ct);
+    }
+}
