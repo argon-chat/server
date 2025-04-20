9b0a3ac514c9b7febbc91c5174b59d871e99067d
Yuuki Wesp <ls-micro@ya.ru>
2025-04-20 17:12:33 +0300

    update cdn s3 options

--- src/Argon.Api/Features/MediaStorage/CdnFeatureExtensions.cs
+++ src/Argon.Api/Features/MediaStorage/CdnFeatureExtensions.cs
@@ -9,6 +9,7 @@ using Genbox.SimpleS3.Extensions.GenericS3.Extensions;
 using Genbox.SimpleS3.Extensions.HttpClient.Extensions;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.DependencyInjection;
 
 public static class CdnFeatureExtensions
 {
@@ -16,61 +17,50 @@ public static class CdnFeatureExtensions
     {
         builder.Services.Configure<CdnOptions>(builder.Configuration.GetSection("Cdn"));
 
-        var opt           = builder.Configuration.GetSection("Cdn:Storage");
-        var options       = builder.Services.Configure<StorageOptions>(opt);
-        var bucketOptions = new StorageOptions();
-        opt.Bind(bucketOptions);
 
         builder.Services.TryAddSingleton<IContentTypeProvider, FileExtensionContentTypeProvider>();
 
-        return bucketOptions.Kind switch
-        {
-            StorageKind.InMemory  => throw new InvalidOperationException(),
-            StorageKind.Disk      => builder.AddContentDeliveryNetwork<DiskContentDeliveryNetwork>(bucketOptions.Kind, bucketOptions),
-            StorageKind.GenericS3 => builder.AddContentDeliveryNetwork<YandexContentDeliveryNetwork>(bucketOptions.Kind, bucketOptions),
-            _                     => throw new ArgumentOutOfRangeException()
-        };
-    }
+        builder.AddContentDeliveryNetwork<DiskContentDeliveryNetwork>();
+        builder.AddContentDeliveryNetwork<YandexContentDeliveryNetwork>();
 
-    public static IServiceCollection AddContentDeliveryNetwork<T>(this WebApplicationBuilder builder, StorageKind keyName, StorageOptions options)
+        return builder.Services;
+    }
+    
+    public static IServiceCollection AddContentDeliveryNetwork<T>(this WebApplicationBuilder builder)
         where T : class, IContentDeliveryNetwork
     {
-        if (keyName == StorageKind.Disk)
-        {
-            builder.Services.AddKeyedSingleton<IContentStorage, DiskContentStorage>(IContentStorage.DiskContentStorageKey);
-            builder.Services.AddSingleton<IContentDeliveryNetwork, T>();
-        }
-        else if (keyName == StorageKind.GenericS3)
-        {
-            builder.Services.AddKeyedSingleton<IContentStorage, S3ContentStorage>(IContentStorage.GenericS3StorageKey);
-            builder.Services.AddSingleton<IContentDeliveryNetwork, T>();
-            builder.AddS3Storage(keyName, options);
-        }
-
+        builder.Services.AddKeyedSingleton<IContentStorage, S3ContentStorage>(IContentStorage.GenericS3StorageKey);
+        builder.Services.AddKeyedSingleton<IContentStorage, DiskContentStorage>(IContentStorage.DiskContentStorageKey);
+        builder.Services.AddSingleton<IContentDeliveryNetwork, T>();
+        builder.AddS3Storage();
         return builder.Services;
     }
 
 
-    public static IServiceCollection AddS3Storage(this WebApplicationBuilder builder, StorageKind keyName, StorageOptions options)
+    public static IServiceCollection AddS3Storage(this WebApplicationBuilder builder)
     {
+        builder.Services.AddKeyedScoped($"GenericS3:container", (provider, _) => GenerateFactoryS3Provider(provider));
+        builder.Services.AddKeyedScoped($"GenericS3:client", (services, _)
+            => services.GetRequiredKeyedService<IServiceProvider>("GenericS3:container").GetRequiredService<IObjectClient>());
+
+        return builder.Services;
+    }
+
+    private static IServiceProvider GenerateFactoryS3Provider(IServiceProvider provider)
+    {
+        var opt              = provider.GetRequiredService<IOptions<StorageOptions>>();
+        var options          = opt.Value;
         var storageContainer = new ServiceCollection();
         var coreBuilder      = SimpleS3CoreServices.AddSimpleS3Core(storageContainer);
         coreBuilder.UseHttpClient();
-        coreBuilder.UseGenericS3(config =>
-        {
-            config.Endpoint    = options.BaseUrl;
-            config.RegionCode  = options.Region;
-            config.Credentials = new StringAccessKey(options.Login, options.Password);
-            config.NamingMode  = NamingMode.PathStyle;
+        coreBuilder.UseGenericS3(config => {
+            config.Endpoint             = options.BaseUrl;
+            config.RegionCode           = options.Region;
+            config.Credentials          = new StringAccessKey(options.Login, options.Password);
+            config.NamingMode           = NamingMode.PathStyle;
             config.PayloadSignatureMode = SignatureMode.FullSignature;
         });
 
-        var storageServices = storageContainer.BuildServiceProvider();
-
-        builder.Services.AddKeyedSingleton<IServiceProvider>($"{keyName}:container", storageServices);
-        builder.Services.AddKeyedSingleton<IObjectClient>($"{keyName}:client", (services, o)
-            => storageServices.GetRequiredService<IObjectClient>());
-
-        return builder.Services;
+        return storageContainer.BuildServiceProvider();
     }
 }
