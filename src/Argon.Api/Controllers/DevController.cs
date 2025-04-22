67ec960a1f63ab64838269882fcf6b0287d4c1d2
Yuuki Wesp <ls-micro@ya.ru>
2025-04-22 22:45:04 +0300

    add additional test method

--- src/Argon.Api/Controllers/DevController.cs
+++ src/Argon.Api/Controllers/DevController.cs
@@ -1,19 +1,24 @@
-#if DEBUG
-    namespace Argon.Api.Controllers;
+//#if DEBUG
+namespace Argon.Api.Controllers;
 
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.SharedGrains;
 
-    [Route("api/[controller]"), ApiController]
-    public class DevController(IGrainFactory grainFactory) : ControllerBase
-    {
-        [HttpGet("{channelId:guid}")]
-        public async Task<ActionResult<List<ArgonMessage>>> GetMessages(Guid channelId, int count, int offset)
-            => await grainFactory.GetGrain<IChannelGrain>(channelId).GetMessages(count, offset);
+[Route("api/[controller]"), ApiController]
+public class DevController(IGrainFactory grainFactory) : ControllerBase
+{
+    [HttpGet("/getMessages/{channelId:guid}")]
+    public async Task<ActionResult<List<ArgonMessage>>> GetMessages(Guid channelId, int count, int offset)
+        => await grainFactory.GetGrain<IChannelGrain>(channelId).GetMessages(count, offset);
 
-        //[HttpPost("{userId:guid}/{channelId:guid}")]
-        //public async Task SendMessage(Guid userId, Guid channelId, ArgonMessage message)
-        //    => await grainFactory.GetGrain<IChannelGrain>(channelId).SendMessage(message.Text);
-    }
+    [HttpGet("getAccessToken/{userId:guid}")]
+    public async Task<IActionResult> GetAccessKey(Guid userId)
+        => Ok(grainFactory.GetGrain<IAccessTokenGrain>(Guid.Empty).GenerateAccessGuidAsync(userId, DateTime.Now, CancellationToken.None));
 
-#endif
+    //[HttpPost("{userId:guid}/{channelId:guid}")]
+    //public async Task SendMessage(Guid userId, Guid channelId, ArgonMessage message)
+    //    => await grainFactory.GetGrain<IChannelGrain>(channelId).SendMessage(message.Text);
+}
+
+//#endif
