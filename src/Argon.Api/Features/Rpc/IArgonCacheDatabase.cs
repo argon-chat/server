fd16099518906cb3d92a50a828a5bfe900d1263f
Yuuki Wesp <ls-micro@ya.ru>
2025-04-30 20:12:17 +0300

    rework redis connection pool

--- src/Argon.Api/Features/Rpc/IArgonCacheDatabase.cs
+++ src/Argon.Api/Features/Rpc/IArgonCacheDatabase.cs
@@ -4,6 +4,8 @@ using System.Text.RegularExpressions;
 using Features.Env;
 using Grpc.Core;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.ObjectPool;
+using Newtonsoft.Json;
 using StackExchange.Redis;
 
 public interface IArgonCacheDatabase
@@ -19,53 +21,82 @@ public interface IArgonCacheDatabase
 
     IAsyncEnumerable<string> ScanKeysAsync(string pattern, CancellationToken ct = default);
 }
- 
-public class RedisArgonCacheDatabase(IConnectionMultiplexer multiplexer, IServer server) : IArgonCacheDatabase
+
+public class RedisArgonCacheDatabase(IRedisPoolConnections pool) : IArgonCacheDatabase
 {
     public Task StringSetAsync(string key, string value, TimeSpan expiration, CancellationToken ct = default)
-        => multiplexer.GetDatabase().StringSetAsync(key, value, expiration).WaitAsync(ct);
+    {
+        using var scope = pool.Rent();
+
+        return scope.GetDatabase().StringSetAsync(key, value, expiration).WaitAsync(ct);
+    }
 
     public Task StringSetAsync(string key, string value, CancellationToken ct = default)
-        => multiplexer.GetDatabase().StringSetAsync(key, value).WaitAsync(ct);
+    {
+        using var scope = pool.Rent();
+        return scope.GetDatabase().StringSetAsync(key, value).WaitAsync(ct);
+    }
 
     public async Task<string?> StringGetAsync(string key, CancellationToken ct = default)
-        => await multiplexer.GetDatabase().StringGetAsync(key).WaitAsync(ct);
+    {
+        using var scope = pool.Rent();
+
+        return await scope.GetDatabase().StringGetAsync(key).WaitAsync(ct);
+    }
 
     public Task KeyDeleteAsync(string key, CancellationToken ct = default)
-        => multiplexer.GetDatabase().KeyDeleteAsync(key).WaitAsync(ct);
+    {
+        using var scope = pool.Rent();
+
+        return scope.GetDatabase().KeyDeleteAsync(key).WaitAsync(ct);
+    }
 
     public Task<bool> KeyExistsAsync(string key, CancellationToken ct = default)
-        => multiplexer.GetDatabase().KeyExistsAsync(key).WaitAsync(ct);
+    {
+        using var scope = pool.Rent();
+
+        return scope.GetDatabase().KeyExistsAsync(key).WaitAsync(ct);
+    }
 
     public async IAsyncEnumerable<string> ScanKeysAsync(string pattern, CancellationToken ct = default)
     {
-        foreach (var key in server.Keys(pattern: pattern))
+        using var scope = pool.Rent();
+        foreach (var key in scope.GetServer().Keys(pattern: pattern, pageSize: 1))
             yield return key;
     }
 
     public async Task<IAsyncDisposable> SubscribeToExpired(Func<string, Task> onKeyExpired, CancellationToken ct = default)
     {
+        var scope = pool.Rent();
+
         var k = new RedisChannel("__keyevent@0__:expired", RedisChannel.PatternMode.Auto);
-        var s = multiplexer.GetSubscriber();
-        var e = new CacheSubscriber(k, s);
+        var s = scope.GetMultiplexer().GetSubscriber();
+        var e = new CacheSubscriber(k, s, scope);
         var w = await s.SubscribeAsync(k);
         w.OnMessage(async message => await onKeyExpired(message.Message.ToString()));
         return e;
     }
 }
 
-public class CacheSubscriber(RedisChannel channelKey, ISubscriber subscriber) : IDisposable, IAsyncDisposable
+public class CacheSubscriber(RedisChannel channelKey, ISubscriber subscriber, ConnectionScope scope) : IDisposable, IAsyncDisposable
 {
     public void Dispose()
-        => subscriber.Unsubscribe(channelKey);
+    {
+        subscriber.Unsubscribe(channelKey);
+        ((IDisposable)scope).Dispose();
+    }
 
     public async ValueTask DisposeAsync()
-        => await subscriber.UnsubscribeAsync(channelKey);
+    {
+        await subscriber.UnsubscribeAsync(channelKey);
+        ((IDisposable)scope).Dispose();
+    }
 }
 
 public sealed class InMemoryArgonCacheDatabase(IDistributedCache cache) : IArgonCacheDatabase
 {
     private static readonly ConcurrentDictionary<string, byte> _keys = new();
+
     public Task StringSetAsync(string key, string value, TimeSpan expiration, CancellationToken ct = default)
     {
         _keys.TryAdd(key, 0);
@@ -78,7 +109,7 @@ public sealed class InMemoryArgonCacheDatabase(IDistributedCache cache) : IArgon
     public Task StringSetAsync(string key, string value, CancellationToken ct = default)
     {
         _keys.TryAdd(key, 0);
-        return cache.SetStringAsync(key, value, token: ct);
+        return cache.SetStringAsync(key, value, ct);
     }
 
     public Task<string?> StringGetAsync(string key, CancellationToken ct = default)
@@ -108,7 +139,7 @@ public sealed class InMemoryArgonCacheDatabase(IDistributedCache cache) : IArgon
         {
             if (regex.IsMatch(key))
                 yield return key;
-            await Task.Yield(); 
+            await Task.Yield();
         }
     }
 
@@ -133,16 +164,117 @@ public static class ArgonCacheDatabaseFeature
         }
         else
         {
-            builder.Services.AddSingleton<IArgonCacheDatabase, RedisArgonCacheDatabase>();
-            builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
-                ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("cache")!));
-            builder.Services.AddSingleton(sp => {
-                var mux = sp.GetRequiredService<IConnectionMultiplexer>();
-                var endpoint = mux.GetEndPoints().First();
-                return mux.GetServer(endpoint);
-            });
+            builder.Services.Configure<RedisConnectionPoolOptions>(builder.Configuration.GetSection("redis"));
+            builder.Services.AddSingleton<IRedisPoolConnections, RedisConnectionPool>();
+            builder.Services.AddHostedService(q => q.GetRequiredService<IRedisPoolConnections>());
+            builder.Services.AddScoped<IArgonCacheDatabase, RedisArgonCacheDatabase>();
         }
 
         return builder.Services;
+    }
+}
+
+public readonly struct ConnectionScope(IConnectionMultiplexer multiplexer, RedisConnectionPool pool) : IDisposable
+{
+    void IDisposable.Dispose()
+        => pool.Return(multiplexer);
+
+    public IServer GetServer()
+        => multiplexer.GetServer(multiplexer.GetEndPoints().First());
+
+    public IConnectionMultiplexer GetMultiplexer() 
+        => multiplexer;
+
+    public IDatabase GetDatabase(int dbId = -1)
+        => multiplexer.GetDatabase(dbId);
+}
+
+public record RedisConnectionPoolOptions
+{
+    public uint MaxSize { get; set; } = 16;
+}
+
+public interface IRedisPoolConnections : IHostedService
+{
+    ConnectionScope Rent();
+}
+
+public class RedisConnectionPool(IConfiguration cfg, ILogger<RedisConnectionPool> logger, IOptions<RedisConnectionPoolOptions> options) : BackgroundService, IRedisPoolConnections
+{
+    private readonly ConcurrentBag<IConnectionMultiplexer> ConnectionPool = new();
+
+    private readonly PeriodicTimer timer = new(TimeSpan.FromMinutes(2));
+
+    private ulong Taken;
+    private ulong Allocated;
+
+
+    public ConnectionScope Rent()
+    {
+        Interlocked.Increment(ref Taken);
+        return ConnectionPool.TryTake(out var multiplexer) ? new ConnectionScope(multiplexer, this) : new ConnectionScope(EnsureNew(), this);
+    }
+
+    public void Return(IConnectionMultiplexer connection)
+    {
+        Interlocked.Decrement(ref Taken);
+        ConnectionPool.Add(connection);
+    }
+
+
+    private IConnectionMultiplexer EnsureNew()
+    {
+        Interlocked.Increment(ref Allocated);
+        return ConnectionMultiplexer.Connect(cfg.GetConnectionString("cache")!);
+    }
+
+    private void Populate()
+    {
+        foreach (var _ in Enumerable.Range(0, (int)Math.Max(6, (int)options.Value.MaxSize / 2u)).Select(_ => EnsureNew())) { }
+    }
+
+    private async Task Cleanup()
+    {
+        var (allocated, taken) = (Interlocked.Read(ref Allocated), Interlocked.Read(ref Taken));
+        var maxSize = options.Value.MaxSize;
+        var excess  = allocated - taken;
+        if (excess <= maxSize) return;
+
+        var rmRequired = excess - maxSize;
+        logger.LogWarning("Cleaning up {Count} excess redis connections", rmRequired);
+
+        while (rmRequired > 0 && ConnectionPool.TryTake(out var mux))
+        {
+            try
+            {
+                await mux.CloseAsync();
+                await mux.DisposeAsync();
+                Interlocked.Decrement(ref Allocated);
+                rmRequired--;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to clean up Redis connection.");
+            }
+        }
+    }
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Populate();
+
+        try
+        {
+            while (await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                var (allocated, taken) = (Interlocked.Read(ref Allocated), Interlocked.Read(ref Taken));
+                logger.LogInformation("Cleaning up redis pool call, currently allocated: {connectionAllocated}, in use: {connectionTaken}", allocated, taken);
+                await Cleanup();
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            logger.LogInformation("Redis cleanup service stopping...");
+        }
     }
 }
