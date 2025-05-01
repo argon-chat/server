5ac9e76647d89ea89a2684213c551bb1ecd14c8d
Yuuki Wesp <ls-micro@ya.ru>
2025-05-01 20:26:15 +0300

    fix

--- src/Argon.Contracts/ArgonEntity.cs
+++ src/Argon.Contracts/ArgonEntity.cs
@@ -23,27 +23,17 @@ public abstract record ArgonEntity<T>
     [System.ComponentModel.DataAnnotations.Key]
     public T Id { get; set; }
 
-    [IgnoreMember, TsIgnore]
     public DateTimeOffset CreatedAt { get; set; }
-    [IgnoreMember, TsIgnore]
     public DateTimeOffset UpdatedAt { get; set; }
-    [IgnoreMember, TsIgnore]
     public DateTimeOffset? DeletedAt { get; set; }
-
-    [IgnoreMember, TsIgnore]
     public bool IsDeleted { get; set; }
 }
 
 [MessagePackObject(true), TsInterface]
 public abstract record ArgonEntityNoKey
 {
-    [IgnoreMember, TsIgnore]
     public DateTimeOffset CreatedAt { get; set; }
-    [IgnoreMember, TsIgnore]
     public DateTimeOffset UpdatedAt { get; set; }
-    [IgnoreMember, TsIgnore]
     public DateTimeOffset? DeletedAt { get; set; }
-
-    [IgnoreMember, TsIgnore]
     public bool IsDeleted { get; set; }
 }
