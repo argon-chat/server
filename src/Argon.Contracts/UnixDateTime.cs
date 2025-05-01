78bf22e661e76a1c4f37bea7836662e55b97fae5
Yuuki Wesp <ls-micro@ya.ru>
2025-05-01 16:47:40 +0300

    try fix?

--- src/Argon.Contracts/UnixDateTime.cs
+++ src/Argon.Contracts/UnixDateTime.cs
@@ -5,5 +5,12 @@ using System;
 public static class UnixDateTime
 {
     public static long ToUnixTimestamp(this DateTime value)
-        => ((DateTimeOffset)value).ToUnixTimeSeconds();
+        => ConvertToUnixTimestamp(value);
+
+    public static long ConvertToUnixTimestamp(DateTime date)
+    {
+        var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        var diff   = date.ToUniversalTime() - origin;
+        return (long)Math.Floor(diff.TotalSeconds);
+    }
 }
