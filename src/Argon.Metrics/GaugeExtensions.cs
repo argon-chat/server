e8b793c5538d5b1f4061aad38ef5b73a26cf80dc
Yuuki Wesp <ls-micro@ya.ru>
2025-06-04 06:48:35 +0300

    drop

--- src/Argon.Metrics/GaugeExtensions.cs
+++ src/Argon.Metrics/GaugeExtensions.cs
@@ -1,7 +1,5 @@
 namespace Argon.Metrics;
 
-using System.Diagnostics;
-
 public static class GaugeExtensions
 {
     public static Task GaugeAsync(this IMetricsCollector collector, MeasurementId name, double value,
@@ -20,33 +18,5 @@ public static class GaugeExtensions
         {
             await metrics.GaugeAsync(name, 0, tags);
         }
-    }
-}
-
-public static class LatencyBucketExtensions
-{
-    public async static Task ObserveLatencyBucketAsync(this IMetricsCollector metrics, MeasurementId name,
-        Func<Task> action, IDictionary<string, string>? tags = null)
-    {
-        var sw = Stopwatch.StartNew();
-        await action();
-        sw.Stop();
-
-        var elapsedMs = sw.Elapsed.TotalMilliseconds;
-        var bucket = elapsedMs switch
-        {
-            < 50   => "<50ms",
-            < 200  => "<200ms",
-            < 1000 => "<1s",
-            _      => "slow"
-        };
-
-        var finalTags = tags == null
-            ? new Dictionary<string, string>()
-            : new Dictionary<string, string>(tags);
-
-        finalTags["latency_bucket"] = bucket;
-
-        await metrics.CountAsync(name, 1, finalTags);
     }
 }
