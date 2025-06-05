6b476beabcb1827e6217c1f7c42f94a196924eb8
Yuuki Wesp <ls-micro@ya.ru>
2025-06-05 13:36:47 +0300

    add dc, node and role

--- src/Argon.Metrics/InfluxMetricsCollector.cs
+++ src/Argon.Metrics/InfluxMetricsCollector.cs
@@ -1,18 +1,28 @@
 namespace Argon.Metrics;
 
 using InfluxDB3.Client.Write;
+using Microsoft.Extensions.DependencyInjection;
 
-public class InfluxMetricsCollector(IPointBuffer writer) : IMetricsCollector
+public class InfluxMetricsCollector(IPointBuffer writer, IServiceProvider provider, IWebHostEnvironment env) : IMetricsCollector
 {
-    private static PointData BuildPoint(MeasurementId measurement, IDictionary<string, string>? tags)
+    private readonly Lazy<string?> Datacenter = new(() => provider.GetKeyedService<string>("dc"));
+
+    private PointData BuildPoint(MeasurementId measurement, IDictionary<string, string>? tags)
     {
         var point = PointData.Measurement(measurement.key);
         if (tags == null) return point;
         foreach (var tag in tags)
             point.SetTag(tag.Key, tag.Value);
+
+        if (Datacenter.Value is { } dc)
+            point.SetTag("dc", dc);
+        if (Environment.GetEnvironmentVariable("ARGON_ROLE") is { } role)
+            point.SetTag("role", role);
+        if (Environment.GetEnvironmentVariable("NODE") is { } node)
+            point.SetTag("node", node);
         return point;
     }
-
+     
     public async Task CountAsync(MeasurementId measurement, long value = 1, IDictionary<string, string>? tags = null)
     {
         var point = BuildPoint(measurement, tags)
