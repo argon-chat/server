b86413ee29e3de8569bd8c899f3af8bdc739b809
Yuuki Wesp <ls-micro@ya.ru>
2025-09-05 21:10:10 +0300

    Add ArgonCouponEntity class with properties and configuration
    A new ArgonCouponEntity class is added with various properties like Code, Description, ValidFrom, ValidTo, MaxRedemptions, RedemptionCount, IsActive, ReferenceItemEntityId along with a collection property Redemptions. The EntityTypeConfiguration for the entity is also defined in the Configure method setting up keys, indexes, property constraints, and relationships.

---
+++ src/Argon.Api/Entities/Data/ArgonCouponEntity.cs
@@ -0,0 +1,49 @@
+namespace Argon.Api.Entities.Data;
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public record ArgonCouponEntity : ArgonEntity, IEntityTypeConfiguration<ArgonCouponEntity>
+{
+    public         string           Code                  { get; set; } = null!;
+    public         string?          Description           { get; set; }
+    public         DateTime         ValidFrom             { get; set; }
+    public         DateTime         ValidTo               { get; set; }
+    public         int              MaxRedemptions        { get; set; }
+    public         int              RedemptionCount       { get; set; }
+    public         bool             IsActive              { get; set; }
+    public         Guid?            ReferenceItemEntityId { get; set; }
+    public virtual ArgonItemEntity? ReferenceItemEntity   { get; set; }
+
+    public ICollection<ArgonCouponRedemptionEntity> Redemptions { get; set; } = new List<ArgonCouponRedemptionEntity>();
+
+    public void Configure(EntityTypeBuilder<ArgonCouponEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Code).IsUnique();
+
+        builder.Property(x => x.Code)
+           .IsRequired()
+           .HasMaxLength(64);
+
+        builder.Property(x => x.Description)
+           .HasMaxLength(512);
+
+        builder.Property(x => x.ValidFrom).IsRequired();
+        builder.Property(x => x.ValidTo).IsRequired();
+        builder.Property(x => x.MaxRedemptions).IsRequired();
+        builder.Property(x => x.RedemptionCount).IsRequired();
+        builder.Property(x => x.IsActive).IsRequired();
+
+        builder.HasOne(x => x.ReferenceItemEntity)
+           .WithMany()
+           .HasForeignKey(x => x.ReferenceItemEntityId)
+           .OnDelete(DeleteBehavior.SetNull);
+
+        builder.HasMany(x => x.Redemptions)
+           .WithOne(x => x.Coupon)
+           .HasForeignKey(x => x.CouponId);
+    }
+}
