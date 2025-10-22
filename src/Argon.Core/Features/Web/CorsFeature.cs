01945c1465393931219c564b0b3f29ed36428582
Yuuki Wesp <ls-micro@ya.ru>
2025-10-22 05:31:04 +0300

    add preflight cache

--- src/Argon.Core/Features/Web/CorsFeature.cs
+++ src/Argon.Core/Features/Web/CorsFeature.cs
@@ -4,23 +4,26 @@ public static class CorsFeature
 {
     public static List<(string scheme, string host)> AllowedHost =
     [
-        ("http", "localhost"),
-        ("https", "localhost"),
-        ("app", "index"),
-        ("https", "argon.gl"),
-        ("https", "argon.zone"),
-        ("https", "link.argon.gl"),
-        ("https", "meet.argon.gl"),
+        ("http", "localhost"), // for testing
+        ("https", "localhost"), // for testing
+        ("app", "index"), // origin in host
+        ("https", "argon.gl"), // base domain
+        ("https", "argon.zone"), // extended apps
+        ("https", "link.argon.gl"), // link app
+        ("https", "meet.argon.gl"), // meet
+        ("https", "x-frontend-development.argon.gl"), // for testing
     ];
+
     public static void AddDefaultCors(this WebApplicationBuilder builder)
         => builder.Services.AddCors(x => x.AddDefaultPolicy(z => z.SetIsOriginAllowed(origin =>
             {
                 var uri = new Uri(origin);
-                return AllowedHost.Any(w => 
-                    uri.Host.Equals(w.host, StringComparison.InvariantCulture) && 
+                return AllowedHost.Any(w =>
+                    uri.Host.Equals(w.host, StringComparison.InvariantCulture) &&
                     uri.Scheme.Equals(w.scheme, StringComparison.InvariantCulture));
             })
            .AllowAnyHeader()
            .AllowAnyMethod()
-           .WithExposedHeaders("X-Wt-Upgrade", "X-Wt-Fingerprint", "X-Wt-AAT")));
+           .WithExposedHeaders("X-Wt-Upgrade", "X-Wt-Fingerprint", "X-Wt-AAT")
+           .SetPreflightMaxAge(TimeSpan.FromDays(1))));
 }
