88954714f7b860c4b32d5bf988dc3dcd4f6d3895
Yuuki Wesp <ls-micro@ya.ru>
2025-11-01 11:40:56 +0300

    Update entity and property names in InviteGrain class. Improve code readability and consistency.

--- src/Argon.Api/Grains/InviteGrain.cs
+++ src/Argon.Api/Grains/InviteGrain.cs
@@ -8,12 +8,12 @@ public class InviteGrain(IDbContextFactory<ApplicationDbContext> context, Hybrid
 {
     public async ValueTask<(Guid, AcceptInviteError)> AcceptAsync()
     {
-        var e = await cache.GetOrCreateAsync<ServerInvite?>(
+        var e = await cache.GetOrCreateAsync<SpaceInvite?>(
             string.Format(InviteCodeEntityData.CacheEntityKey, this.GetPrimaryKeyString()), async token =>
         {
             await using var db = await context.CreateDbContextAsync(token);
 
-            var entity = await db.ServerInvites.AsNoTracking().FirstOrDefaultAsync(
+            var entity = await db.Invites.AsNoTracking().FirstOrDefaultAsync(
                 x => x.Id == InviteCodeEntityData.EncodeToUlong(this.GetPrimaryKeyString()),
                 token);
             return entity;
@@ -21,7 +21,7 @@ public class InviteGrain(IDbContextFactory<ApplicationDbContext> context, Hybrid
 
         if (e is null)
             return (Guid.Empty, AcceptInviteError.NOT_FOUND);
-        if (e.Expired < DateTime.Now)
+        if (e.ExpireAt < DateTime.Now)
             return (Guid.Empty, AcceptInviteError.EXPIRED);
         await GrainFactory.GetGrain<ISpaceGrain>(e.SpaceId).DoJoinUserAsync();
         return (e.SpaceId, AcceptInviteError.NONE);
