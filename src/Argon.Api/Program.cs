db3c57a00e930e8ee84567edbda3a24225da3ad5
Yuuki Wesp <ls-micro@ya.ru>
2025-11-01 22:02:47 +0300

    bump

--- src/Argon.Api/Program.cs
+++ src/Argon.Api/Program.cs
@@ -1,23 +1,9 @@
 using Argon.Api.Features.Bus;
-using Argon.Api.Features.Utils;
-using Argon.Api.Migrations;
+using Argon.Core.Features.EF;
 using Argon.Features.Env;
 using Argon.Features.HostMode;
 using Argon.Features.RegionalUnit;
 using Argon.Services.Ion;
-using Npgsql;
-
-AppContext.SetSwitch("Npgsql.EnablePreparedStatements", false);
-//var       cs   = "Host=localhost;Port=5433;Username=postgres;Password=postgres;Database=argon-chat-v2;Include Error Detail=true;ConnectionIdleLifetime=15;ConnectionPruningInterval=10"; // замени под себя
-//using var conn = new NpgsqlConnection(cs);
-//conn.Open();
-
-//using var cmd = new NpgsqlCommand("SELECT pg_typeof(@p)", conn);
-//var       p   = new NpgsqlParameter("p", new DateOnly(2004, 4, 8));
-//cmd.Parameters.Add(p);
-
-//var result = cmd.ExecuteScalar();
-//Console.WriteLine(result);
 
 var builder = await RegionalUnitApp.CreateBuilder(args);
 if (builder.Environment.IsSingleInstance())
@@ -28,7 +14,8 @@ else
     builder.AddMultiRegionWorkloads();
 builder.Services.AddSingleton<SubscriptionController>();
 
-builder.Services.AddIonProtocol((x) => {
+builder.Services.AddIonProtocol((x) =>
+{
     x.AddInterceptor<ArgonTransactionInterceptor>();
     x.AddInterceptor<ArgonOrleansInterceptor>();
     x.AddService<IUserInteraction, UserInteractionImpl>();
@@ -49,11 +36,6 @@ else if (builder.Environment.IsSingleRegion())
     app.UseSingleRegionWorkloads();
 else
     app.UseMultiRegionWorkloads();
-
-app.Use(async (context, func) =>
-{
-    await func(context);
-});
 
 await app.WarmUpCassandra();
 await app.WarmUpRotations();
