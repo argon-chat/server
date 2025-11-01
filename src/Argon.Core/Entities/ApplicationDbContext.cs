271e5d7131502721eafbe127b225d0a2e66a9216
Yuuki Wesp <ls-micro@ya.ru>
2025-11-01 11:42:17 +0300

    Update DbContext constructor parameters and entity names, add region options. Refactor model configurations for multi-region database usage, unsigned long compatibility, and soft delete handling. Seed default user and space data with updated descriptions and creation dates.

--- src/Argon.Core/Entities/ApplicationDbContext.cs
+++ src/Argon.Core/Entities/ApplicationDbContext.cs
@@ -1,60 +1,54 @@
 namespace Argon.Entities;
 
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System.Drawing;
-using System.Linq.Expressions;
 using Api.Entities.Data;
+using Argon.Features.EF;
+using System.Drawing;
 using static ArgonEntitlement;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IOptions<DatabaseRegionOptions> regionOptions) : DbContext(options)
 {
     public DbSet<UserEntity>                        Users                        => Set<UserEntity>();
     public DbSet<UserDeviceHistoryEntity>           DeviceHistories              => Set<UserDeviceHistoryEntity>();
-    public DbSet<UserAgreements>                    UserAgreements               => Set<UserAgreements>();
-    public DbSet<SpaceEntity>                       Servers                      => Set<SpaceEntity>();
+    public DbSet<SpaceEntity>                       Spaces                       => Set<SpaceEntity>();
     public DbSet<ChannelEntity>                     Channels                     => Set<ChannelEntity>();
     public DbSet<SpaceMemberEntity>                 UsersToServerRelations       => Set<SpaceMemberEntity>();
-    public DbSet<SpaceMemberArchetypeEntity>        ServerMemberArchetypes       => Set<SpaceMemberArchetypeEntity>();
+    public DbSet<SpaceMemberArchetypeEntity>        MemberArchetypes             => Set<SpaceMemberArchetypeEntity>();
     public DbSet<ArchetypeEntity>                   Archetypes                   => Set<ArchetypeEntity>();
     public DbSet<ChannelEntitlementOverwriteEntity> ChannelEntitlementOverwrites => Set<ChannelEntitlementOverwriteEntity>();
-    public DbSet<ServerInvite>                      ServerInvites                => Set<ServerInvite>();
+    public DbSet<SpaceInvite>                       Invites                      => Set<SpaceInvite>();
     public DbSet<ArgonMessageEntity>                Messages                     => Set<ArgonMessageEntity>();
     public DbSet<UserProfileEntity>                 UserProfiles                 => Set<UserProfileEntity>();
     public DbSet<UsernameReservedEntity>            Reservation                  => Set<UsernameReservedEntity>();
-    public DbSet<SpaceCategoryEntity>               Categories                   => Set<SpaceCategoryEntity>();
     public DbSet<ArgonItemEntity>                   Items                        => Set<ArgonItemEntity>();
     public DbSet<ArgonItemNotificationEntity>       UnreadInventoryItems         => Set<ArgonItemNotificationEntity>();
     public DbSet<ArgonCouponEntity>                 Coupons                      => Set<ArgonCouponEntity>();
     public DbSet<ArgonCouponRedemptionEntity>       CouponRedemption             => Set<ArgonCouponRedemptionEntity>();
 
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        => configurationBuilder.Conventions.Add(_ => new DefaultStringColumnTypeConvention());
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        modelBuilder.UseMultiRegionDatabase(regionOptions.Value.PrimaryRegion, regionOptions.Value.ReplicateRegion);
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        modelBuilder.UseUnsignedLongCompatibility();
+        modelBuilder.UseSoftDeleteCompatibility();
 
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            if (!typeof(ArgonEntity).IsAssignableFrom(entityType.ClrType))
-                continue;
-            modelBuilder.Entity(entityType.ClrType)
-               .HasQueryFilter(GetSoftDeleteFilter(entityType.ClrType));
-        }
 
         modelBuilder.Entity<UserEntity>().HasData(new UserEntity
         {
-            Username           = "system",
-            NormalizedUsername = "system",
-            DisplayName        = "System",
-            Email              = "system@argon.gl",
-            Id                 = UserEntity.SystemUser,
-            PasswordDigest     = null
+            Username       = "system",
+            DisplayName    = "System",
+            Email          = "system@argon.gl",
+            Id             = UserEntity.SystemUser,
+            PasswordDigest = null
         });
 
         modelBuilder.Entity<SpaceEntity>().HasData(new SpaceEntity
         {
             Name      = "system_server",
             CreatorId = UserEntity.SystemUser,
-            Id        = SpaceEntity.DefaultSystemServer
+            Id        = SpaceEntity.DefaultSystemSpace
         });
 
         modelBuilder.Entity<ArchetypeEntity>().HasData([
@@ -66,13 +60,13 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
                 Entitlement = ViewChannel | ReadHistory | JoinToVoice | SendMessages | SendVoice | AttachFiles | AddReactions | AnyMentions |
                               MentionEveryone | ExternalEmoji | ExternalStickers | UseCommands | PostEmbeddedLinks | Connect | Speak | Video |
                               Stream,
-                SpaceId       = SpaceEntity.DefaultSystemServer,
+                SpaceId       = SpaceEntity.DefaultSystemSpace,
                 Name          = "everyone",
                 IsLocked      = false,
                 IsMentionable = true,
                 IsHidden      = false,
-                Description   = "Default role for everyone in this server",
-                CreatedAt     = new DateTime(2024, 11, 23, 16, 1, 14, 205, DateTimeKind.Utc).AddTicks(8411)
+                Description   = "Default role for everyone in this space",
+                CreatedAt     = new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddTicks(0)
             }
         ]);
 
@@ -83,50 +77,14 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
                 Colour        = Color.Gray,
                 CreatorId     = UserEntity.SystemUser,
                 Entitlement   = ArgonEntitlementKit.Administrator,
-                SpaceId       = SpaceEntity.DefaultSystemServer,
+                SpaceId       = SpaceEntity.DefaultSystemSpace,
                 Name          = "owner",
                 IsLocked      = true,
                 IsMentionable = false,
                 IsHidden      = true,
-                Description   = "Default role for owner in this server",
-                CreatedAt     = new DateTime(2024, 11, 23, 16, 1, 14, 205, DateTimeKind.Utc).AddTicks(8382)
+                Description   = "Default role for owner in this space",
+                CreatedAt     = new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddTicks(0)
             }
         ]);
-
-        var dateTimeOffsetConverter = new ValueConverter<DateTimeOffset, long>(
-            v => v.ToUnixTimeMilliseconds(),
-            v => DateTimeOffset.FromUnixTimeMilliseconds(v)
-        );
-
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            foreach (var property in entityType.GetProperties())
-            {
-                if (property.ClrType == typeof(DateTimeOffset))
-                    property.SetValueConverter(dateTimeOffsetConverter);
-            }
-        }
-
-        var dateTimeOffsetNullConverter = new ValueConverter<DateTimeOffset?, long?>(
-            v => (v == null ? null : v.Value.ToUnixTimeMilliseconds()),
-            v => v == null ? null : DateTimeOffset.FromUnixTimeMilliseconds(v.Value)
-        );
-
-        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
-        {
-            foreach (var property in entityType.GetProperties())
-            {
-                if (property.ClrType == typeof(DateTimeOffset?))
-                    property.SetValueConverter(dateTimeOffsetNullConverter);
-            }
-        }
-    }
-
-    private static LambdaExpression GetSoftDeleteFilter(Type type)
-    {
-        var parameter         = Expression.Parameter(type, "e");
-        var isDeletedProperty = Expression.Property(parameter, nameof(ArgonEntity.IsDeleted));
-        var notDeleted        = Expression.Not(isDeletedProperty);
-        return Expression.Lambda(notDeleted, parameter);
     }
 }
