5294a7c8843822252c38c6cc2c87508143a2abbb
Yuuki Wesp <ls-micro@ya.ru>
2025-11-01 11:41:01 +0300

    Refactor invite handling in ServerInviteGrain
    - Renamed ServerInvites to Invites for clarity
    - Updated property names for consistency

--- src/Argon.Api/Grains/ServerInviteGrain.cs
+++ src/Argon.Api/Grains/ServerInviteGrain.cs
@@ -11,12 +11,12 @@ public class ServerInviteGrain(ILogger<IServerInvitesGrain> logger, IDbContextFa
         await using var db         = await context.CreateDbContextAsync();
         var             inviteCode = InviteCodeEntityData.GenerateInviteCode();
 
-        await db.ServerInvites.AddAsync(new ServerInvite
+        await db.Invites.AddAsync(new SpaceInvite
         {
             Id        = InviteCodeEntityData.EncodeToUlong(inviteCode),
             CreatedAt = DateTime.Now,
             CreatorId = issuer,
-            Expired   = DateTime.UtcNow + expiration,
+            ExpireAt   = DateTime.UtcNow + expiration,
             SpaceId  = this.GetPrimaryKey(),
         });
         await db.SaveChangesAsync();
@@ -27,10 +27,10 @@ public class ServerInviteGrain(ILogger<IServerInvitesGrain> logger, IDbContextFa
     {
         await using var db = await context.CreateDbContextAsync();
 
-        var list = await db.ServerInvites
+        var list = await db.Invites
            .Where(x => x.SpaceId == this.GetPrimaryKey())
            .AsNoTracking()
            .ToListAsync();
-        return list.Select(x => new InviteCodeEntityData(new InviteCode(InviteCodeEntityData.DecodeFromUlong(x.Id)), x.SpaceId, x.CreatorId, x.Expired, 0)).ToList();
+        return list.Select(x => new InviteCodeEntityData(new InviteCode(InviteCodeEntityData.DecodeFromUlong(x.Id)), x.SpaceId, x.CreatorId, x.ExpireAt, 0)).ToList();
     }
 }
