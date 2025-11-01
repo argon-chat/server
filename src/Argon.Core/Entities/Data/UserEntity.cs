fd233c49e878c6cb74b0a91d04cdff06dc051033
Yuuki Wesp <ls-micro@ya.ru>
2025-11-01 11:42:08 +0300

    Refactor UserEntity properties and add normalization logic
    - Refactored property definitions in UserEntity class
    - Added NormalizedEmail and NormalizedUsername properties with normalization logic
    - Updated Configure method to include property configurations for Email and Username

--- src/Argon.Core/Entities/Data/UserEntity.cs
+++ src/Argon.Core/Entities/Data/UserEntity.cs
@@ -9,25 +9,14 @@ public record UserEntity : ArgonEntity, IMapper<UserEntity, ArgonUser>, IEntityT
     public static readonly Guid SystemUser
         = Guid.Parse("11111111-2222-1111-2222-111111111111");
 
-    [MaxLength(255)]
-    public required string Email { get; set; }
-    [MaxLength(64)]
-    public required string Username { get; set; }
-    [MaxLength(64)]
-    public required string NormalizedUsername { get; set; }
-    [MaxLength(64)]
-    public required string DisplayName { get; set; }
-    [MaxLength(64)]
-    public string? PhoneNumber { get; set; } = null;
-    [MaxLength(512)]
-    public string? PasswordDigest { get; set; } = null;
-    [MaxLength(128)]
-    public string? AvatarFileId { get; set; } = null;
+    public required string  Email          { get; set; }
+    public required string  Username       { get; set; }
+    public required string  DisplayName    { get; set; }
+    public          string? PhoneNumber    { get; set; } = null;
+    public          string? PasswordDigest { get; set; } = null;
+    public          string? AvatarFileId   { get; set; } = null;
 
     public ICollection<SpaceMemberEntity> ServerMembers { get; set; } = new List<SpaceMemberEntity>();
-
-    public LockdownReason LockdownReason { get; set; }
-    public DateTimeOffset? LockDownExpiration { get; set; }
 
     public DateOnly? DateOfBirth { get; set; }
 
@@ -39,13 +28,45 @@ public record UserEntity : ArgonEntity, IMapper<UserEntity, ArgonUser>, IEntityT
 
     public virtual UserProfileEntity Profile { get; set; }
 
+
+    public string NormalizedEmail    { get; private set; } = null!;
+    public string NormalizedUsername { get; private set; } = null!;
+
+    public bool AllowedSendOptionalEmails { get; set; }
+    public bool AgreeTOS                  { get; set; }
+
+
+
+
+    public LockdownReason  LockdownReason       { get; set; }
+    public DateTimeOffset? LockDownExpiration   { get; set; }
+    public bool            LockDownIsAppealable { get; set; }
+
     public static ArgonUser Map(scoped in UserEntity self)
         => new(self.Id, self.Username, self.DisplayName, self.AvatarFileId);
 
     public void Configure(EntityTypeBuilder<UserEntity> builder)
     {
         builder.HasKey(x => x.Id);
+        builder.Property(x => x.Email)
+           .IsRequired()
+           .HasMaxLength(255);
+
+        builder.Property(x => x.Username)
+           .IsRequired()
+           .HasMaxLength(64);
+
+        builder.Property(x => x.NormalizedEmail)
+           .HasColumnType("varchar(255)")
+           .HasComputedColumnSql("lower(\"Email\")", true)
+           .ValueGeneratedOnAddOrUpdate();
+
+        builder.Property(x => x.NormalizedUsername)
+           .HasColumnType("varchar(64)")
+           .HasComputedColumnSql("lower(\"Username\")", true)
+           .ValueGeneratedOnAddOrUpdate();
+
+        builder.HasIndex(x => x.NormalizedEmail).IsUnique();
         builder.HasIndex(x => x.NormalizedUsername).IsUnique();
-        builder.HasIndex(x => x.Email).IsUnique();
     }
 }
