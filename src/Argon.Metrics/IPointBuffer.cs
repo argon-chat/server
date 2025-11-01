9f24dae610dc8102a415441a84fe3f2fc9e571b8
Yuuki Wesp <ls-micro@ya.ru>
2025-11-01 11:44:57 +0300

    drop project with metrics

--- src/Argon.Metrics/IPointBuffer.cs
+++
@@ -1,8 +0,0 @@
-namespace Argon.Metrics;
-
-using InfluxDB3.Client.Write;
-
-public interface IPointBuffer
-{
-    void Enqueue(PointData point);
-}
--- src/Argon.Metrics/InfluxDbOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Argon.Metrics;
-
-public class InfluxDbOptions
-{
-    public required bool    IsEnabled { get; set; } = false;
-    public required string  Url       { get; set; }
-    public required string  Token     { get; set; }
-    public          string? Database  { get; set; }
-}
