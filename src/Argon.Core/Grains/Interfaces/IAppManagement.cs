6cb33bd4f14e4da3ff00b2b2292c0c7370138d28
Yuuki Wesp <ls-micro@ya.ru>
2025-11-11 23:22:05 +0300

    add AppsManagementGrain

--- src/Argon.Core/Grains/Interfaces/IAppManagement.cs
+++ src/Argon.Core/Grains/Interfaces/IAppManagement.cs
@@ -1,8 +1,19 @@
 namespace Argon.Grains.Interfaces;
 
-[Alias("Argon.Grains.Interfaces.IAppManagement")]
-public interface IAppManagement : IGrainWithGuidKey
+[Alias("Argon.Grains.Interfaces.IAppsManagementGrain")]
+public interface IAppsManagementGrain : IGrainWithGuidKey
 {
     //[Alias(nameof(CreateTeamAsync))]
     //Task<Guid> CreateTeamAsync(Guid ownerId, string name, CancellationToken ct = default);
+
+    [Alias("GetScopesForBotAsync")]
+    Task<Either<List<string>, GetScopesForBotError>> GetScopesForBotAsync(string clientId, string clientSecret, string redirect, CancellationToken ct = default);
+}
+
+
+public enum GetScopesForBotError
+{
+    NoBotFound,
+    BadSecret,
+    BadRedirectsUrls
 }
