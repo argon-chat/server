215980f9265ffe1eb629bd4c47d7d316ac8d23d9
Yuuki Wesp <ls-micro@ya.ru>
2025-11-11 23:34:45 +0300

    bump

--- src/Argon.Core/Grains/Interfaces/IAppManagement.cs
+++ src/Argon.Core/Grains/Interfaces/IAppManagement.cs
@@ -6,14 +6,14 @@ public interface IAppsManagementGrain : IGrainWithGuidKey
     //[Alias(nameof(CreateTeamAsync))]
     //Task<Guid> CreateTeamAsync(Guid ownerId, string name, CancellationToken ct = default);
 
-    [Alias("GetScopesForBotAsync")]
-    Task<Either<List<string>, GetScopesForBotError>> GetScopesForBotAsync(string clientId, string clientSecret, string redirect, CancellationToken ct = default);
+    [Alias("GetCredentialsForBotAsync")]
+    Task<BotCredentialsInfo> GetCredentialsForBotAsync(string clientId, CancellationToken ct = default);
 }
 
+public record BotCredentialsInfo(
+    string ClientId,
+    string ClientSecret,
+    List<string> allowedRedirects,
+    List<string> scopes,
+    bool IsAllowedRefreshToken);
 
-public enum GetScopesForBotError
-{
-    NoBotFound,
-    BadSecret,
-    BadRedirectsUrls
-}
