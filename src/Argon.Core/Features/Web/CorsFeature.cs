077d8dfbf9968ff06de31d179d1c2fc6ee905741
Yuuki Wesp <ls-micro@ya.ru>
2025-11-18 03:49:25 +0300

    add additional cors

--- src/Argon.Core/Features/Web/CorsFeature.cs
+++ src/Argon.Core/Features/Web/CorsFeature.cs
@@ -4,26 +4,45 @@ public static class CorsFeature
 {
     public static List<(string scheme, string host)> AllowedHost =
     [
-        ("http" , "localhost"),                       // for testing
-        ("https", "localhost"),                       // for testing
-        ("app"  , "index"),                           // origin in host
-        ("https", "argon.gl"),                        // base domain
-        ("https", "argon.zone"),                      // extended apps
-        ("https", "link.argon.gl"),                   // link app
-        ("https", "meet.argon.gl"),                   // meet
-        ("https", "aegis.argon.gl"),                  // identity
-        ("https", "console.argon.gl"),                // identity
-        ("https", "x-frontend-development.argon.gl"), // for testing
-        ("https", "local.argon.gl")                   // for testing
+        ("http", "localhost"),
+        ("https", "localhost"),
+
+        ("app", "index"),
+
+        ("https", "argon.gl"),
+        ("https", "argon.zone"),
+
+        ("https", "link.argon.gl"),
+        ("https", "meet.argon.gl"),
+        ("https", "aegis.argon.gl"),
+        ("https", "console.argon.gl"),
+        ("https", "argx.argon.gl"),
+        ("https", "k3sd.argon.gl"),
+        ("https", "vault.argon.gl"),
+        ("https", "x-frontend-development.argon.gl"),
+        ("https", "local.argon.gl"),
+
+        ("https", "www.jwt.io"),
+        ("https", "jwt.io"),
+
     ];
 
     public static void AddDefaultCors(this WebApplicationBuilder builder)
         => builder.Services.AddCors(x => x.AddDefaultPolicy(z => z.SetIsOriginAllowed(origin =>
             {
                 var uri = new Uri(origin);
+
                 return AllowedHost.Any(w =>
-                    uri.Host.Equals(w.host, StringComparison.InvariantCulture) &&
-                    uri.Scheme.Equals(w.scheme, StringComparison.InvariantCulture));
+                {
+                    if (!uri.Scheme.Equals(w.scheme, StringComparison.InvariantCulture))
+                        return false;
+
+                    if (uri.Host.Equals(w.host, StringComparison.InvariantCulture))
+                        return true;
+
+                    return w.host == "argon.gl" &&
+                           (uri.Host.EndsWith(".argon.gl", StringComparison.InvariantCulture));
+                });
             })
            .AllowAnyHeader()
            .AllowAnyMethod()
