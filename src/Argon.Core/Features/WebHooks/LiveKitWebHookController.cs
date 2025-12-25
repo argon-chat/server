0fec2c0ddaccf5f44a78ffb13c413e722893101b
Yuuki Wesp <ls-micro@ya.ru>
2025-12-25 21:38:10 +0300

    remove fucking code from webhook

--- src/Argon.Core/Features/WebHooks/LiveKitWebHookController.cs
+++ src/Argon.Core/Features/WebHooks/LiveKitWebHookController.cs
@@ -9,27 +9,6 @@ public class LiveKitWebHookController(ILogger<LiveKitWebHookController> logger, 
     [HttpPost("/webhook-endpoint")]
     public async Task<IActionResult> Webhook([FromBody] JToken webhookEvent)
     {
-        logger.LogInformation("Called livekit webhook, event: {json}", webhookEvent.ToString());
-
-        var @event = webhookEvent.ToObject<LiveKitWebhookEvent>();
-
-        if (@event is null)
-            throw new Exception($"@event is null");
-
-
-        if (@event.Event.Equals("participant_left"))
-        {
-            var userId    = @event.Participant.Identity;
-            var channelId = string.Join("", @event.Room.Name.Skip(37).Take(36));
-
-            await client.GetGrain<IChannelGrain>(Guid.Parse(channelId)).Leave(Guid.Parse(userId));
-        }
-        else if (@event.Event.Equals("room_finished"))
-        {
-            var channelId = string.Join("", @event.Room.Name.Skip(37).Take(36));
-            await client.GetGrain<IChannelGrain>(Guid.Parse(channelId)).ClearChannel();
-        }
-
         return Ok();
     }
 }
