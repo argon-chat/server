2ff2b6e9cfd57a4ed5bd475392081c104f73082e
Yuuki Wesp <ls-micro@ya.ru>
2026-05-12 17:55:11 +0300

    also strip X-Forwarded-Tls-Client-Cert too

--- src/Argon.Core/Features/Middlewares/ForwardedHeadersExtensions.cs
+++ src/Argon.Core/Features/Middlewares/ForwardedHeadersExtensions.cs
@@ -2,6 +2,7 @@ namespace Argon.Features.Middlewares;
 
 using System.Net;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpOverrides;
 
 public static class ForwardedHeadersExtensions
@@ -11,19 +12,14 @@ public static class ForwardedHeadersExtensions
     /// <summary>
     /// Configures forwarded headers with trusted proxy networks from configuration.
     /// Reads CIDRs from "ForwardedHeaders:KnownNetworks" section, defaults to K3s pod/service CIDRs.
+    /// Also strips X-Forwarded-Tls-Client-Cert from untrusted sources.
     /// </summary>
     public static WebApplication UseConfiguredForwardedHeaders(this WebApplication app)
     {
-        var options = new ForwardedHeadersOptions
-        {
-            ForwardedHeaders = ForwardedHeaders.XForwardedFor
-                             | ForwardedHeaders.XForwardedHost
-                             | ForwardedHeaders.XForwardedProto
-        };
-
         var cidrs = app.Configuration.GetSection("ForwardedHeaders:KnownNetworks").Get<string[]>()
                     ?? DefaultKnownNetworks;
 
+        var trustedNetworks = new List<System.Net.IPNetwork>();
         foreach (var cidr in cidrs)
         {
             var parts = cidr.Split('/');
@@ -31,11 +27,48 @@ public static class ForwardedHeadersExtensions
                 && IPAddress.TryParse(parts[0], out var address)
                 && int.TryParse(parts[1], out var prefixLength))
             {
-                options.KnownIPNetworks.Add(new System.Net.IPNetwork(address, prefixLength));
+                trustedNetworks.Add(new System.Net.IPNetwork(address, prefixLength));
             }
         }
 
+        var options = new ForwardedHeadersOptions
+        {
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor
+                             | ForwardedHeaders.XForwardedHost
+                             | ForwardedHeaders.XForwardedProto
+        };
+
+        foreach (var network in trustedNetworks)
+            options.KnownIPNetworks.Add(network);
+
         app.UseForwardedHeaders(options);
+
+        // Strip X-Forwarded-Tls-Client-Cert from requests not originating from trusted proxy networks
+        app.Use((context, next) =>
+        {
+            if (context.Request.Headers.ContainsKey("X-Forwarded-Tls-Client-Cert"))
+            {
+                var remoteIp = context.Connection.RemoteIpAddress;
+                if (remoteIp is null || !IsInTrustedNetwork(remoteIp, trustedNetworks))
+                    context.Request.Headers.Remove("X-Forwarded-Tls-Client-Cert");
+            }
+            return next(context);
+        });
+
         return app;
+    }
+
+    private static bool IsInTrustedNetwork(IPAddress address, List<System.Net.IPNetwork> networks)
+    {
+        // Normalize IPv4-mapped IPv6 to IPv4
+        if (address.IsIPv4MappedToIPv6)
+            address = address.MapToIPv4();
+
+        foreach (var network in networks)
+        {
+            if (network.Contains(address))
+                return true;
+        }
+        return false;
     }
 }
