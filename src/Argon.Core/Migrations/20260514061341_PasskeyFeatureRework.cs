1c0f17eb26fbe538bd05d6566c3491aadbf17c76
Yuuki Wesp <ls-micro@ya.ru>
2026-05-14 09:19:50 +0300

    restore passkey table

---
+++ src/Argon.Core/Migrations/20260514061341_PasskeyFeatureRework.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Argon.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasskeyFeatureRework : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Passkeys",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", maxLength: 128, nullable: false),
+                    CredentialId = table.Column<byte[]>(type: "bytea", maxLength: 1024, nullable: true),
+                    PublicKey = table.Column<byte[]>(type: "bytea", maxLength: 2048, nullable: true),
+                    SignCount = table.Column<long>(type: "bigint", nullable: false),
+                    AaGuid = table.Column<Guid>(type: "uuid", nullable: true),
+                    LastUsedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passkeys", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Passkeys_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passkeys_CredentialId",
+                table: "Passkeys",
+                column: "CredentialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Passkeys_UserId",
+                table: "Passkeys",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Passkeys");
+        }
+    }
+}
